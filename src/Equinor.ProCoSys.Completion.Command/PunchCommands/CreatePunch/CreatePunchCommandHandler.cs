edaa164b7f77583ef40968a3ba0573e8cf9d598e
Tord Joranger <TORDJ@equinor.com>
2023-06-30 11:27:26 +0200

    remove unused code

--- src/Equinor.ProCoSys.Completion.Command/PunchCommands/CreatePunch/CreatePunchCommandHandler.cs
+++ src/Equinor.ProCoSys.Completion.Command/PunchCommands/CreatePunch/CreatePunchCommandHandler.cs
@@ -8,7 +8,6 @@ using Equinor.ProCoSys.Completion.Domain.AggregateModels.ProjectAggregate;
 using MediatR;
 using ServiceResult;
 using Equinor.ProCoSys.Common.Misc;
-using Equinor.ProCoSys.Completion.Command.EventHandlers.DomainEvents.PunchEvents;
 using Equinor.ProCoSys.Completion.Domain.Events.DomainEvents.PunchEvents;
 using MassTransit;
 
@@ -22,22 +21,19 @@ public class CreatePunchCommandHandler : IRequestHandler<CreatePunchCommand, Res
     private readonly IPunchRepository _punchRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IProjectRepository _projectRepository;
-    private readonly IPublishEndpoint _publishEndpoint;
 
     public CreatePunchCommandHandler(
         IPlantProvider plantProvider,
         IPunchRepository punchRepository,
         IUnitOfWork unitOfWork,
         IProjectRepository projectRepository,
-        ILogger<CreatePunchCommandHandler> logger, 
-        IPublishEndpoint publishEndpoint)
+        ILogger<CreatePunchCommandHandler> logger)
     {
         _plantProvider = plantProvider;
         _punchRepository = punchRepository;
         _unitOfWork = unitOfWork;
         _projectRepository = projectRepository;
         _logger = logger;
-        _publishEndpoint = publishEndpoint;
     }
 
     public async Task<Result<GuidAndRowVersion>> Handle(CreatePunchCommand request, CancellationToken cancellationToken)
@@ -50,9 +46,6 @@ public class CreatePunchCommandHandler : IRequestHandler<CreatePunchCommand, Res
 
         var punch = new Punch(_plantProvider.Plant, project, request.ItemNo);
         _punchRepository.Add(punch);
-        
-        //await PublishDistributed(new PunchCreated(punch,request.ProjectGuid), cancellationToken);
-        
         punch.AddDomainEvent(new PunchCreatedEvent(punch, request.ProjectGuid));
 
         await _unitOfWork.SaveChangesAsync(cancellationToken);
@@ -60,17 +53,5 @@ public class CreatePunchCommandHandler : IRequestHandler<CreatePunchCommand, Res
         _logger.LogInformation("Punch '{PunchItemNo}' with guid {PunchGuid} created", punch.ItemNo, punch.Guid);
 
         return new SuccessResult<GuidAndRowVersion>(new GuidAndRowVersion(punch.Guid, punch.RowVersion.ConvertToString()));
-    }
-
-    private async Task PublishDistributed(PunchCreatedIntegrationEvent punchCreatedIntegrationEvent, CancellationToken cancellationToken)
-    {
-        var sessionId = punchCreatedIntegrationEvent.Guid.ToString();
-        await _publishEndpoint.Publish(punchCreatedIntegrationEvent,
-            context =>
-            {
-                context.SetSessionId(sessionId);
-                _logger.LogInformation("Published: {Message}", context.Message.ToString());
-            },
-            cancellationToken);
     }
 }
