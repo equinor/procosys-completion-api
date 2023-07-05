44d19b5726424666b3aab99e3f07971bb989db60
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-07-05 14:02:14 +0200

    Added missing test

---
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.Tests/Authorizations/IsPunchItemCommandTests/AccessValidatorForClearPunchItemCommandTests.cs
@@ -0,0 +1,14 @@
+﻿using Equinor.ProCoSys.Completion.Command.PunchItemCommands.UpdatePunchItem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.Completion.WebApi.Tests.Authorizations.IsPunchItemCommandTests;
+
+[TestClass]
+public class AccessValidatorForUpdatePunchItemCommandTests : AccessValidatorForIIsPunchItemCommandTests<UpdatePunchItemCommand>
+{
+    protected override UpdatePunchItemCommand GetPunchItemCommandWithAccessToProject()
+        => new(PunchItemGuidWithAccessToProject, null!, null!);
+
+    protected override UpdatePunchItemCommand GetPunchItemCommandWithoutAccessToProject()
+        => new(PunchItemGuidWithoutAccessToProject, null!, null!);
+}
