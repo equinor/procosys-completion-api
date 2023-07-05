e0c3e00aec2e5a06ac47312c23348e3754a4e945
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-07-05 14:30:28 +0200

    renames

--- src/Equinor.ProCoSys.Completion.Command/PunchItemCommands/ClearPunchItem/ClearPunchItemCommandValidator.cs
+++ src/Equinor.ProCoSys.Completion.Command/PunchItemCommands/ClearPunchItem/ClearPunchItemCommandValidator.cs
@@ -8,31 +8,31 @@ namespace Equinor.ProCoSys.Completion.Command.PunchItemCommands.ClearPunchItem;
 
 public class ClearPunchItemCommandValidator : AbstractValidator<ClearPunchItemCommand>
 {
-    public ClearPunchItemCommandValidator(IPunchItemValidator punchValidator)
+    public ClearPunchItemCommandValidator(IPunchItemValidator punchItemValidator)
     {
         RuleLevelCascadeMode = CascadeMode.Stop;
         ClassLevelCascadeMode = CascadeMode.Stop;
 
         RuleFor(command => command)
-            .MustAsync((command, cancellationToken) => NotBeInAClosedProjectForPunchAsync(command.PunchItemGuid, cancellationToken))
+            .MustAsync((command, cancellationToken) => NotBeInAClosedProjectForPunchItemAsync(command.PunchItemGuid, cancellationToken))
             .WithMessage("Project is closed!")
-            .MustAsync((command, cancellationToken) => BeAnExistingPunchAsync(command.PunchItemGuid, cancellationToken))
+            .MustAsync((command, cancellationToken) => BeAnExistingPunchItemAsync(command.PunchItemGuid, cancellationToken))
             .WithMessage(command => $"Punch item with this guid does not exist! Guid={command.PunchItemGuid}")
-            .MustAsync((command, cancellationToken) => NotBeInAVoidedTagForPunchAsync(command.PunchItemGuid, cancellationToken))
+            .MustAsync((command, cancellationToken) => NotBeInAVoidedTagForPunchItemAsync(command.PunchItemGuid, cancellationToken))
             .WithMessage("Tag owning punch item is voided!")
             .MustAsync((command, cancellationToken) => IsReadyToBeClearedAsync(command.PunchItemGuid, cancellationToken))
             .WithMessage(command => $"Punch item can not be cleared! Guid={command.PunchItemGuid}");
 
-        async Task<bool> NotBeInAClosedProjectForPunchAsync(Guid punchItemGuid, CancellationToken cancellationToken)
-            => !await punchValidator.ProjectOwningPunchItemIsClosedAsync(punchItemGuid, cancellationToken);
+        async Task<bool> NotBeInAClosedProjectForPunchItemAsync(Guid punchItemGuid, CancellationToken cancellationToken)
+            => !await punchItemValidator.ProjectOwningPunchItemIsClosedAsync(punchItemGuid, cancellationToken);
 
-        async Task<bool> NotBeInAVoidedTagForPunchAsync(Guid punchItemGuid, CancellationToken cancellationToken)
-            => !await punchValidator.TagOwningPunchItemIsVoidedAsync(punchItemGuid, cancellationToken);
+        async Task<bool> NotBeInAVoidedTagForPunchItemAsync(Guid punchItemGuid, CancellationToken cancellationToken)
+            => !await punchItemValidator.TagOwningPunchItemIsVoidedAsync(punchItemGuid, cancellationToken);
 
-        async Task<bool> BeAnExistingPunchAsync(Guid punchItemGuid, CancellationToken cancellationToken)
-            => await punchValidator.ExistsAsync(punchItemGuid, cancellationToken);
+        async Task<bool> BeAnExistingPunchItemAsync(Guid punchItemGuid, CancellationToken cancellationToken)
+            => await punchItemValidator.ExistsAsync(punchItemGuid, cancellationToken);
 
         async Task<bool> IsReadyToBeClearedAsync(Guid punchItemGuid, CancellationToken cancellationToken)
-            => await punchValidator.IsReadyToBeClearedAsync(punchItemGuid, cancellationToken);
+            => await punchItemValidator.IsReadyToBeClearedAsync(punchItemGuid, cancellationToken);
     }
 }
