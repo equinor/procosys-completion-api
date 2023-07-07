48545d4323aca059741f44cb9d1d11e31cc686f6
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-07-07 07:21:23 +0200

    Unit tests
    (cherry picked from commit fac4ce90de26dd14df7198b7e34b0aa26115a02d)

---
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.Tests/Authorizations/IsPunchItemCommandTests/AccessValidatorForRejectPunchItemCommandTests.cs
@@ -0,0 +1,14 @@
+﻿using Equinor.ProCoSys.Completion.Command.PunchItemCommands.RejectPunchItem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.Completion.WebApi.Tests.Authorizations.IsPunchItemCommandTests;
+
+[TestClass]
+public class AccessValidatorForRejectPunchItemCommandTests : AccessValidatorForIIsPunchItemCommandTests<RejectPunchItemCommand>
+{
+    protected override RejectPunchItemCommand GetPunchItemCommandWithAccessToProject()
+        => new(PunchItemGuidWithAccessToProject, null!);
+
+    protected override RejectPunchItemCommand GetPunchItemCommandWithoutAccessToProject()
+        => new(PunchItemGuidWithoutAccessToProject, null!);
+}
