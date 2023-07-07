c77912e331b71614a110af852226e8507e709fb2
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-07-07 12:12:35 +0200

    PunchItemUnclearedEventHandler, publishing PunchItemUnclearedIntegrationEvent based on contract IPunchItemUnclearedV1

---
+++ src/Equinor.ProCoSys.Completion.Command/EventHandlers/DomainEvents/PunchItemEvents/PunchItemUnclearedEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Completion.Command.EventHandlers.DomainEvents.PunchItemEvents.IntegrationEvents;
+using Equinor.ProCoSys.Completion.Domain.Events.DomainEvents.PunchItemDomainEvents;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Equinor.ProCoSys.Completion.Command.EventHandlers.DomainEvents.PunchItemEvents;
+
+public class PunchItemUnclearedEventHandler : INotificationHandler<PunchItemUnclearedDomainEvent>
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+    private readonly ILogger<PunchItemUnclearedEventHandler> _logger;
+
+    public PunchItemUnclearedEventHandler(IPublishEndpoint publishEndpoint, ILogger<PunchItemUnclearedEventHandler> logger)
+    {
+        _publishEndpoint = publishEndpoint;
+        _logger = logger;
+    }
+
+    public async Task Handle(PunchItemUnclearedDomainEvent punchItemUnclearedEvent, CancellationToken cancellationToken)
+    {
+        var sessionId = punchItemUnclearedEvent.PunchItem.Guid.ToString();
+
+        await _publishEndpoint.Publish(new PunchItemUnclearedIntegrationEvent(punchItemUnclearedEvent),
+            context =>
+            {
+                context.SetSessionId(sessionId);
+                _logger.LogInformation("Publishing: {Message}", context.Message.ToString());
+            },
+            cancellationToken);
+    }
+}
