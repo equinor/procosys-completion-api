7668c913a8c3acf282337ed797ebd3d2d38a0e17
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-07-07 07:19:26 +0200

    Reject Command, Validation and handling
    (cherry picked from commit 96ae7f954c7b88a122f0eeda7b9f042d33227308)

---
+++ src/Equinor.ProCoSys.Completion.Command/PunchItemCommands/RejectPunchItem/RejectPunchItemCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.ProCoSys.Completion.Command.PunchItemCommands.RejectPunchItem;
+
+public class RejectPunchItemCommand : IRequest<Result<string>>, IIsPunchItemCommand
+{
+    public RejectPunchItemCommand(Guid punchItemGuid, string rowVersion)
+    {
+        PunchItemGuid = punchItemGuid;
+        RowVersion = rowVersion;
+    }
+
+    public Guid PunchItemGuid { get; }
+    public string RowVersion { get; }
+}
