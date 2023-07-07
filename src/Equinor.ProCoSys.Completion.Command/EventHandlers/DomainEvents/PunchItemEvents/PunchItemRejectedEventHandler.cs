b7e172c0ecdd5ea125f021bd461507aba6bed4f5
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-07-07 07:21:16 +0200

    Publishing and handling PunchItemRejectedDomainEvent and PunchItemRejectedIntegrationEvent with contract IPunchItemRejectedV1
    (cherry picked from commit e6b48d012c8a71defdf72b56b08b1f00f904d8eb)

---
+++ src/Equinor.ProCoSys.Completion.Command/EventHandlers/DomainEvents/PunchItemEvents/PunchItemRejectedEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Completion.Command.EventHandlers.DomainEvents.PunchItemEvents.IntegrationEvents;
+using Equinor.ProCoSys.Completion.Domain.Events.DomainEvents.PunchItemDomainEvents;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Equinor.ProCoSys.Completion.Command.EventHandlers.DomainEvents.PunchItemEvents;
+
+public class PunchItemRejectedEventHandler : INotificationHandler<PunchItemRejectedDomainEvent>
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+    private readonly ILogger<PunchItemRejectedEventHandler> _logger;
+
+    public PunchItemRejectedEventHandler(IPublishEndpoint publishEndpoint, ILogger<PunchItemRejectedEventHandler> logger)
+    {
+        _publishEndpoint = publishEndpoint;
+        _logger = logger;
+    }
+
+    public async Task Handle(PunchItemRejectedDomainEvent punchItemRejectedEvent, CancellationToken cancellationToken)
+    {
+        var sessionId = punchItemRejectedEvent.PunchItem.Guid.ToString();
+
+        await _publishEndpoint.Publish(new PunchItemRejectedIntegrationEvent(punchItemRejectedEvent),
+            context =>
+            {
+                context.SetSessionId(sessionId);
+                _logger.LogInformation("Publishing: {Message}", context.Message.ToString());
+            },
+            cancellationToken);
+    }
+}
