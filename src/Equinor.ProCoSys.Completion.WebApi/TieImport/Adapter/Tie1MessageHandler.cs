226343baaee606ac7e1f740cdd6113f08350ddbb
Jostein Søiland <jsoi@equinor.com>
2023-08-16 14:53:55 +0200

    Clean up message handler

--- src/Equinor.ProCoSys.Completion.WebApi/TieImport/Adapter/Tie1MessageHandler.cs
+++ src/Equinor.ProCoSys.Completion.WebApi/TieImport/Adapter/Tie1MessageHandler.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,29 +8,15 @@ using Equinor.TI.TIE.Adapter.Base.Message;
 using Equinor.TI.TIE.Adapter.TIE1.Message;
 using Microsoft.Extensions.Logging;
 using Statoil.TI.InterfaceServices.Message;
-//using AdapterConsoleApp.ApplicationInsights;
-//using CommonServiceLocator;
-//using Import.ProCoSys;
 
 namespace Equinor.ProCoSys.Completion.WebApi.TieImport.Adapter;
 
 public class Tie1MessageHandler : IMessageHandler<TieAdapterConfig, TieAdapterPartitionConfig, Tie1Message, Tie1Receipt>
 {
     private readonly ILogger<Tie1MessageHandler> _logger;
-    //private readonly ITelemetryHelper _telemetryHelper;
-    //private readonly IImportHandler _importHandler;
 
-    private readonly Stopwatch _processingTimeStopwatch = new Stopwatch();
-
-    public Tie1MessageHandler(
-        ILogger<Tie1MessageHandler> logger
-        //ITelemetryHelper telemetryHelper
-    )
-    {
-        _logger = logger;
-        //_telemetryHelper = telemetryHelper;
-        //_importHandler = ServiceLocator.Current.GetInstance<IImportHandler>();
-    }
+    public Tie1MessageHandler(ILogger<Tie1MessageHandler> logger) 
+        => _logger = logger;
 
     public Task<MessageHandleResult<Tie1Receipt>> HandleSinglePerPartition(
         TieAdapterConfig config,
@@ -50,37 +35,16 @@ public class Tie1MessageHandler : IMessageHandler<TieAdapterConfig, TieAdapterPa
 
     private MessageHandleResult<Tie1Receipt> HandleMessage(Tie1Message message)
     {
-        _processingTimeStopwatch.Restart();
-
+        //TODO: Add custom application insights tracking 
         //_telemetryHelper.TrackMessageReceivedEvent(message.Message);
+
         _logger.LogInformation($"Got message with GUID={message.Message.Guid} ({message.Message.Site})");
 
-        // Pass message to the internal import handler logic.
-        //var responseFrame = _importHandler.Handle(WrapInMessageFrame(message));
-
-
-        // Get the handling result (there will only ever by one result as we are handling messages one by one).
-        //var result = responseFrame.Results.First();
-
-        _processingTimeStopwatch.Stop();
-        //_telemetryHelper.TrackMessageProcessedEvent(
-        //    message.Message,
-        //    GetReceiptStatus(result).ToString(),
-        //    _processingTimeStopwatch.ElapsedMilliseconds,
-        //    result.Logs);
-
-        // Create and return receipt.
-        //var receipt = Tie1Receipt.Create(message.Message, GetReceiptStatus(result), GetReceiptComment(result));
-        //receipt.Logs = GetReceiptLogs(result, message.Message.Site);
-
-        //return new MessageHandleResult<Tie1Receipt>
-        //{
-        //    Receipt = receipt
-        //};
+        //TODO: Route message to handling code and obtain a result from the handling
 
         return new MessageHandleResult<Tie1Receipt>
         {
-                
+            //Empty for now
         };
     }
 
@@ -116,7 +80,7 @@ public class Tie1MessageHandler : IMessageHandler<TieAdapterConfig, TieAdapterPa
         var logs = new List<Log>
         {
             // default log statement
-            new Log
+            new()
             {
                 LogType = LogTypes.Information,
                 Text = $"Processed by the adapter handling site {site}."
@@ -171,6 +135,9 @@ public class Tie1MessageHandler : IMessageHandler<TieAdapterConfig, TieAdapterPa
             ? parsedLogType
             : LogTypes.Unknown;
 
-    private static TIMessageFrame WrapInMessageFrame(Tie1Message message)
-        => new TIMessageFrame {Messages = new List<TIInterfaceMessage> {message.Message}};
+    private static TIMessageFrame WrapInMessageFrame(Tie1Message message) 
+        => new()
+        {
+            Messages = new List<TIInterfaceMessage> {message.Message}
+        };
 }
