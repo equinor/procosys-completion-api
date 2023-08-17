b762bbf3105c6ae660998e003a6c78f05f3d929b
Jostein Søiland <jsoi@equinor.com>
2023-08-17 13:06:00 +0200

    Remove unused methods.

--- src/Equinor.ProCoSys.Completion.WebApi/TieImport/Adapter/Tie1MessageHandler.cs
+++ src/Equinor.ProCoSys.Completion.WebApi/TieImport/Adapter/Tie1MessageHandler.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Equinor.ProCoSys.Completion.WebApi.TieImport.Configuration;
 using Equinor.TI.TIE.Adapter.Base.Message;
 using Equinor.TI.TIE.Adapter.TIE1.Message;
 using Microsoft.Extensions.Logging;
-using Statoil.TI.InterfaceServices.Message;
 
 namespace Equinor.ProCoSys.Completion.WebApi.TieImport.Adapter;
 
@@ -53,97 +49,4 @@ public class Tie1MessageHandler : IMessageHandler<TieAdapterConfig, TieAdapterPa
             //Empty for now
         };
     }
-
-    private static ReceiptStatus GetReceiptStatus(TIMessageResult result)
-    {
-        if (result.Result == MessageResults.Successful)
-        {
-            return ResultHasWarnings(result) ? ReceiptStatus.Warning : ReceiptStatus.Successful;
-        }
-
-        return ReceiptStatus.Failed;
-    }
-
-    private static string GetReceiptComment(TIMessageResult result)
-    {
-        switch (result.Result)
-        {
-            case MessageResults.Successful:
-                {
-                    return ResultHasWarnings(result)
-                        ? $"Message processed OK with warnings: {GetWarningsFromResult(result)}"
-                        : "Message processed OK.";
-                }
-            case MessageResults.Failed:
-                return string.IsNullOrEmpty(result.ErrorMessage) ? "Failed. See log(s)." : result.ErrorMessage;
-            default:
-                return "Message processed.";
-        }
-    }
-
-    private static List<Log> GetReceiptLogs(TIMessageResult result, string site)
-    {
-        var logs = new List<Log>
-        {
-            // default log statement
-            new()
-            {
-                LogType = LogTypes.Information,
-                Text = $"Processed by the adapter handling site {site}."
-            }
-        };
-
-        if (result.Logs.Any())
-        {
-            // We will only return the first 100 log entries due to TIE performance issues.
-            const int LogCount = 100;
-
-            logs.AddRange(
-                result.Logs.Take(LogCount).Select(logEntry =>
-                    new Log
-                    {
-                        LogType = ConvertToEnumLogType(logEntry.LogType),
-                        Text = logEntry.LogDescription
-                    }));
-
-            if (result.Logs.Count > LogCount)
-            {
-                logs.Add(new Log
-                {
-                    LogType = LogTypes.Information,
-                    Text = $"{result.Logs.Count - LogCount} more log entries were truncated."
-                });
-            }
-        }
-
-        return logs;
-    }
-
-    private static bool ResultHasWarnings(TIMessageResult result)
-        => result.Logs.Any(log => ConvertToEnumLogType(log.LogType) == LogTypes.Warning);
-
-    private static string GetWarningsFromResult(TIMessageResult result)
-    {
-        if (!ResultHasWarnings(result))
-        {
-            return "No warnings found.";
-        }
-
-        var warningLogs = result.Logs
-            .Where(log => ConvertToEnumLogType(log.LogType) == LogTypes.Warning)
-            .Select(log => log.LogDescription);
-
-        return string.Join(" | ", warningLogs);
-    }
-
-    private static LogTypes ConvertToEnumLogType(string logType)
-        => Enum.TryParse(logType, true, out LogTypes parsedLogType)
-            ? parsedLogType
-            : LogTypes.Unknown;
-
-    private static TIMessageFrame WrapInMessageFrame(Tie1Message message) 
-        => new()
-        {
-            Messages = new List<TIInterfaceMessage> {message.Message}
-        };
 }
