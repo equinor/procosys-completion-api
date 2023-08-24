5530dfa3779188d13936946403e804ff6aa7f81e
Jostein Søiland <jsoi@equinor.com>
2023-08-24 13:43:51 +0200

    Add unit tests for NumberConverter

---
+++ src/Equinor.ProCoSys.Completion.TieImport.Tests/NumberConverterTests.cs
@@ -0,0 +1,28 @@
+﻿using Equinor.ProCoSys.Completion.TieImport.Infrastructure;
+
+namespace Equinor.ProCoSys.Completion.TieImport.Tests;
+
+[TestClass]
+public class NumberConverterTests
+{
+    [TestMethod]
+    public void ConvertToDecimal_ShouldReturnNull_WhenUnableToParseInput()
+    {
+        var result = NumberConverter.ConvertToDecimal("nonsense input");
+        Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    public void ConvertToDecimal_ShouldReturnNumber_WhenInputHasCommaSeparator()
+    {
+        var result = NumberConverter.ConvertToDecimal("123,45");
+        Assert.AreEqual(123.45m, result);
+    }
+
+    [TestMethod]
+    public void ConvertToDecimal_ShouldReturnNumber_WhenInputHasPointSeparator()
+    {
+        var result = NumberConverter.ConvertToDecimal("123.45");
+        Assert.AreEqual(123.45m, result);
+    }
+}
