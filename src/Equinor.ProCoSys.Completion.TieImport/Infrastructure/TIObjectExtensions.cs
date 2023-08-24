1eb1af7fc20bbdc02a59ec098492e4327032a0f2
Jostein Søiland <jsoi@equinor.com>
2023-08-24 14:10:50 +0200

    Add unit tests for TIObjectExtensions

--- src/Equinor.ProCoSys.Completion.TieImport/Infrastructure/TIObjectExtensions.cs
+++ src/Equinor.ProCoSys.Completion.TieImport/Infrastructure/TIObjectExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Equinor.ProCoSys.Completion.TieImport.Extensions;
-using Statoil.TI.InterfaceServices.Message;
+﻿using Statoil.TI.InterfaceServices.Message;
 
 namespace Equinor.ProCoSys.Completion.TieImport.Infrastructure;
 public static class TIObjectExtensions
@@ -41,69 +40,4 @@ public static class TIObjectExtensions
         }
         return $"{logPrefix} ";
     }
-
-    public static bool IsClass(this TIBaseObject tiObject, params string[] classes)
-        => tiObject?.ObjectClass != null &&
-           classes.Any(c => tiObject.ObjectClass.Equals(c, StringComparison.InvariantCultureIgnoreCase));
-
-    private static bool IsMethod(TIBaseObject tiObject, params string[] methods)
-        => methods.Any(m => tiObject.Method.Equals(m, StringComparison.InvariantCultureIgnoreCase));
-
-    public static bool IsMethodCreate(this TIBaseObject tiObject)
-        => IsMethod(tiObject, "Create", "Insert", "Allocate");
-
-    public static bool IsMethodUpdate(this TIBaseObject tiObject) => IsMethod(tiObject, "Update");
-
-    public static bool IsMethodModify(this TIBaseObject tiObject) =>
-        !tiObject.IsMethodCreate() && !tiObject.IsMethodUpdate() && !tiObject.IsMethodDelete();
-
-    public static bool IsMethodDelete(this TIBaseObject tiObject) => IsMethod(tiObject, "Delete");
-
-    public static TIAttribute? GetAttributeCaseInsensitive(this TIBaseObject tiObject, string attributeName)
-        => tiObject?.Attributes == null || string.IsNullOrWhiteSpace(attributeName)
-            ? null
-            : tiObject
-                .Attributes
-                .FirstOrDefault(a => a.Name.Equals(attributeName, StringComparison.InvariantCultureIgnoreCase));
-
-    public static string? GetAttributeValueAsString(this TIBaseObject tiObject, string attributeName) =>
-        tiObject.GetAttributeCaseInsensitive(attributeName)!.GetValueAsString();
-
-    public static string? GetAttributeValueAsStringUpperCase(this TIBaseObject tiObject, string attributeName)
-        => tiObject.GetAttributeCaseInsensitive(attributeName)!.GetValueAsStringUpperCase();
-
-    public static DateTime? GetAttributeValueAsDateTime(this TIBaseObject tiObject, string attributeName)
-        => tiObject.GetAttributeCaseInsensitive(attributeName)!.GetValueAsDateTime();
-
-    public static bool? GetAttributeValueAsBool(this TIBaseObject tiObject, string attributeName)
-        => tiObject.GetAttributeCaseInsensitive(attributeName)!.GetValueAsBool();
-
-    public static double? GetAttributeValueAsDouble(this TIBaseObject tiObject, string attributeName)
-        => tiObject.GetAttributeCaseInsensitive(attributeName)!.GetValueAsDouble();
-
-    public static bool HasAttributeValue(this TIBaseObject tiObject, string attributeName)
-        => tiObject.GetAttributeCaseInsensitive(attributeName)!.HasValue();
-
-    public static bool HasAttributeValueBlankingSignal(this TIBaseObject tiObject, string attributeName)
-        => tiObject.GetAttributeCaseInsensitive(attributeName)!.HasBlankingSignal();
-
-    public static bool
-        AttributeValueIsDifferentFrom(this TIBaseObject tiObject, string attributeName, string attributeValue)
-        => HasAttributeValue(tiObject, attributeName) && !GetAttributeValueAsString(tiObject, attributeName)!
-               .Equals(attributeValue, StringComparison.InvariantCulture);
-
-    public static TIRelationship? GetRelationship(this TIObject tiObject, string relationshipName)
-        => tiObject.Relationships == null || string.IsNullOrWhiteSpace(relationshipName)
-            ? null
-            : tiObject
-                .Relationships
-                .FirstOrDefault(a => a.Name.Equals(relationshipName, StringComparison.InvariantCultureIgnoreCase));
-
-    public static TISubObject? GetSubObject(this TIObject tiObject, string subObjectClass)
-        => tiObject.SubObjects == null || string.IsNullOrWhiteSpace(subObjectClass)
-            ? null
-            : tiObject
-                .SubObjects
-                .FirstOrDefault(a =>
-                    a.ObjectClass.Equals(subObjectClass, StringComparison.InvariantCultureIgnoreCase));
 }
