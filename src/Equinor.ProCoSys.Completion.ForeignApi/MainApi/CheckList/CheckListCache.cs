2ba0830369ca56a3ee328bc86e67c6a2d236b200
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-09-05 11:26:20 +0200

    Added ForeignApi project to read checklist data from Main (while Main still is master for CheckList data)

---
+++ src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/CheckList/CheckListCache.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Common.Caches;
+
+namespace Equinor.ProCoSys.Completion.ForeignApi.MainApi.CheckList;
+public class CheckListCache : ICheckListCache
+{
+    private readonly ICacheManager _cacheManager;
+    private readonly ICheckListApiService _checkListApiService;
+    private readonly int _checkListCacheInMinutes = 20;
+
+    public CheckListCache(ICacheManager cacheManager, ICheckListApiService checkListApiService)
+    {
+        _cacheManager = cacheManager;
+        _checkListApiService = checkListApiService;
+    }
+
+    public async Task<ProCoSys4CheckList?> GetCheckListAsync(string plant, Guid checkListGuid)
+        => await _cacheManager.GetOrCreate(
+            CheckListGuidCacheKey(checkListGuid),
+            async () =>
+            {
+                var checkList = await _checkListApiService.GetCheckListAsync(plant, checkListGuid);
+                return checkList;
+            },
+            CacheDuration.Minutes,
+            _checkListCacheInMinutes);
+
+    private string CheckListGuidCacheKey(Guid checkListGuid)
+        => $"CHECKLIST_{checkListGuid.ToString().ToUpper()}";
+}
---
+++ src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/CheckList/ICheckListApiService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace Equinor.ProCoSys.Completion.ForeignApi.MainApi.CheckList;
+
+public interface ICheckListApiService
+{
+    Task<ProCoSys4CheckList?> GetCheckListAsync(string plant, Guid checkListGuid);
+}
