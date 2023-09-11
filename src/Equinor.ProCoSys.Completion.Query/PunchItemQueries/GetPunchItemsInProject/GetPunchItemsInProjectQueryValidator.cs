4f40a38dad828ba97863be0fd621a34432edc91f
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-09-11 07:23:57 +0200

    Added GetPunchItemsInProjectQueryValidator
    (cherry picked from commit 216c31111f1e80e5ebe418ad7bc6e7eb98671d3c)
    (cherry picked from commit cb64d1a415529e44eb9ed3730bf0dc204a007b34)

---
+++ src/Equinor.ProCoSys.Completion.Query/PunchItemQueries/GetPunchItemsInProject/GetPunchItemsInProjectQueryValidator.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Completion.Domain.Validators;
+using FluentValidation;
+
+namespace Equinor.ProCoSys.Completion.Query.PunchItemQueries.GetPunchItemsInProject;
+
+public class GetPunchItemsInProjectQueryValidator : AbstractValidator<GetPunchItemsInProjectQuery>
+{
+    public GetPunchItemsInProjectQueryValidator(IProjectValidator projectValidator)
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
+        // todo add unit tests
+        RuleFor(command => command)
+            // validate given Project
+            .MustAsync(BeAnExistingProjectAsync)
+            .WithMessage(command => $"Project does not exist! Guid={command.ProjectGuid}");
+
+        async Task<bool> BeAnExistingProjectAsync(GetPunchItemsInProjectQuery query, CancellationToken cancellationToken)
+            => await projectValidator.ExistsAsync(query.ProjectGuid, cancellationToken);
+    }
+}
