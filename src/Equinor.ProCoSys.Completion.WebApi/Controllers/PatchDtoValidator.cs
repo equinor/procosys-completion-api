521f36fbc2e65404746ccfd13631ee81f1bbd652
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-09-18 14:54:22 +0200

    cleanup

--- src/Equinor.ProCoSys.Completion.WebApi/Controllers/PatchDtoValidator.cs
+++ src/Equinor.ProCoSys.Completion.WebApi/Controllers/PatchDtoValidator.cs
@@ -21,8 +21,6 @@ public class PatchDtoValidator<T1, T2> : AbstractValidator<T1> where T1 : PatchD
             .WithMessage("All operation paths must be unique")
             .Must(RequiredFieldsMustHaveValue)
             .WithMessage(dto => GetMessageForRequiredFields(dto.PatchDocument))
-            //.Must(HaveValidRowVersionOperation)
-            //.WithMessage("'RowVersion' is required and must be a valid row version")
             .Must(HaveValidOperationsOnly)
             .WithMessage(dto => GetMessageForInvalidOperations(dto.PatchDocument))
             .Must(HaveValidLengthOfStrings)
@@ -33,9 +31,6 @@ public class PatchDtoValidator<T1, T2> : AbstractValidator<T1> where T1 : PatchD
 
         bool HaveUniqueReplaceOperations(JsonPatchDocument<T2> doc)
             => patchOperationValidator.HaveUniqueReplaceOperations(doc.Operations);
-
-        //bool HaveValidRowVersionOperation(JsonPatchDocument<T2> doc)
-        //    => patchOperationValidator.HaveValidRowVersionOperation(doc.Operations);
 
         string? GetMessageForInvalidOperations(JsonPatchDocument<T2> doc)
             => patchOperationValidator.GetMessageForInvalidReplaceOperations(doc.Operations);
