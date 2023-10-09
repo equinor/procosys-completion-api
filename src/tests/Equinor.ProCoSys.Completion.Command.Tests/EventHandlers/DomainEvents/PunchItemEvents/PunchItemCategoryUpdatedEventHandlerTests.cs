4440edba771495b9fb8dcd1c6204d0b32eb3b57c
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-10-09 13:57:27 +0200

    unit tests

---
+++ src/tests/Equinor.ProCoSys.Completion.Command.Tests/EventHandlers/DomainEvents/PunchItemEvents/PunchItemCategoryUpdatedEventHandlerTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Completion.Command.EventHandlers.DomainEvents.PunchItemEvents;
+using Equinor.ProCoSys.Completion.Command.EventHandlers.DomainEvents.PunchItemEvents.IntegrationEvents;
+using Equinor.ProCoSys.Completion.Domain.Events;
+using Equinor.ProCoSys.Completion.Domain.Events.DomainEvents.PunchItemDomainEvents;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Equinor.ProCoSys.Completion.Command.Tests.EventHandlers.DomainEvents.PunchItemEvents;
+
+[TestClass]
+public class PunchItemCategoryUpdatedEventHandlerTests : EventHandlerTestBase
+{
+    private PunchItemCategoryUpdatedEventHandler _dut;
+    private PunchItemCategoryUpdatedDomainEvent _punchItemUpdatedEvent;
+    private IPublishEndpoint _publishEndpointMock;
+    private PunchItemUpdatedIntegrationEvent _publishedIntegrationEvent;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _punchItem.SetModified(_person);
+
+        _punchItemUpdatedEvent = new PunchItemCategoryUpdatedDomainEvent(_punchItem, new Property<string>("A", "1", "2"));
+        _publishEndpointMock = Substitute.For<IPublishEndpoint>();
+        _dut = new PunchItemCategoryUpdatedEventHandler(_publishEndpointMock, Substitute.For<ILogger<PunchItemCategoryUpdatedEventHandler>>());
+        _publishEndpointMock
+            .When(x => x.Publish(Arg.Any<PunchItemUpdatedIntegrationEvent>(), Arg.Any<IPipe<PublishContext<PunchItemUpdatedIntegrationEvent>>>()))
+            .Do(info =>
+            {
+                var evt = info.Arg<PunchItemUpdatedIntegrationEvent>();
+                _publishedIntegrationEvent = evt;
+            });
+    }
+
+    [TestMethod]
+    public async Task Handle_ShouldPublish_PunchItemUpdatedIntegrationEvent()
+    {
+        // Act
+        await _dut.Handle(_punchItemUpdatedEvent, default);
+
+        // Assert
+        await _publishEndpointMock.Received(1)
+            .Publish(Arg.Any<PunchItemUpdatedIntegrationEvent>(),
+                Arg.Any<IPipe<PublishContext<PunchItemUpdatedIntegrationEvent>>>());
+    }
+
+    [TestMethod]
+    public async Task Handle_ShouldPublish_CorrectIntegrationEvent()
+    {
+        // Act
+        await _dut.Handle(_punchItemUpdatedEvent, default);
+
+        // Assert
+        Assert.IsNotNull(_publishedIntegrationEvent);
+        Assert.AreEqual($"Punch item changed to {_punchItemUpdatedEvent.PunchItem.Category}", _publishedIntegrationEvent.DisplayName);
+        Assert.AreEqual(_punchItemUpdatedEvent.PunchItem.Guid, _publishedIntegrationEvent.Guid);
+        Assert.AreEqual(_punchItemUpdatedEvent.PunchItem.ModifiedAtUtc, _publishedIntegrationEvent.ModifiedAtUtc);
+        Assert.AreEqual(_punchItemUpdatedEvent.PunchItem.ModifiedBy!.Guid, _publishedIntegrationEvent.ModifiedByOid);
+        Assert.AreEqual(1, _publishedIntegrationEvent.Changes.Count);
+        Assert.AreEqual(_punchItemUpdatedEvent.Change, _publishedIntegrationEvent.Changes.ElementAt(0));
+    }
+}
