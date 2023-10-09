5a5dc11b16183a86ba84df2553e0e64482a40159
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-10-09 13:45:07 +0200

    UpdatePunchItemCategoryCommand with Handling and Validation

---
+++ src/Equinor.ProCoSys.Completion.Command/PunchItemCommands/UpdatePunchItemCategory/UpdatePunchItemCategoryCommandValidator.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Equinor.ProCoSys.Completion.Domain.AggregateModels.PunchItemAggregate;
+using Equinor.ProCoSys.Completion.Domain.Validators;
+using FluentValidation;
+
+namespace Equinor.ProCoSys.Completion.Command.PunchItemCommands.UpdatePunchItemCategory;
+
+public class UpdatePunchItemCategoryCommandValidator : AbstractValidator<UpdatePunchItemCategoryCommand>
+{
+    // Business Validation is based on that Input Validation is done in advance, thus all replaced ..
+    // ... guid values are validated to be Guids
+    public UpdatePunchItemCategoryCommandValidator(IPunchItemValidator punchItemValidator)
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+        ClassLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(command => command)
+            .MustAsync((command, cancellationToken) => NotBeInAClosedProjectForPunchItemAsync(command.PunchItemGuid, cancellationToken))
+            .WithMessage("Project is closed!")
+            .MustAsync((command, cancellationToken) => BeAnExistingPunchItemAsync(command.PunchItemGuid, cancellationToken))
+            .WithMessage(command => $"Punch item with this guid does not exist! Guid={command.PunchItemGuid}")
+            .MustAsync((command, cancellationToken) => NotBeInAVoidedTagForPunchItemAsync(command.PunchItemGuid, cancellationToken))
+            .WithMessage("Tag owning punch item is voided!")
+            .MustAsync((command, cancellationToken) => NotAlreadyBeClearedAsync(command.PunchItemGuid, cancellationToken))
+            .WithMessage(command => $"Punch item is already cleared! Guid={command.PunchItemGuid}")
+            .MustAsync((command, cancellationToken) => NotHaveCategoryAsync(command.PunchItemGuid, command.Category, cancellationToken))
+            .WithMessage(command => $"Punch item already have category {command.Category}! Guid={command.PunchItemGuid}");
+
+        async Task<bool> NotBeInAClosedProjectForPunchItemAsync(Guid punchItemGuid, CancellationToken cancellationToken)
+            => !await punchItemValidator.ProjectOwningPunchItemIsClosedAsync(punchItemGuid, cancellationToken);
+
+        async Task<bool> NotBeInAVoidedTagForPunchItemAsync(Guid punchItemGuid, CancellationToken cancellationToken)
+            => !await punchItemValidator.TagOwningPunchItemIsVoidedAsync(punchItemGuid, cancellationToken);
+
+        async Task<bool> BeAnExistingPunchItemAsync(Guid punchItemGuid, CancellationToken cancellationToken)
+            => await punchItemValidator.ExistsAsync(punchItemGuid, cancellationToken);
+
+        async Task<bool> NotAlreadyBeClearedAsync(Guid punchItemGuid, CancellationToken cancellationToken)
+            => !await punchItemValidator.IsClearedAsync(punchItemGuid, cancellationToken);
+
+        async Task<bool> NotHaveCategoryAsync(Guid punchItemGuid, Category category, CancellationToken cancellationToken)
+            => !await punchItemValidator.HasCategoryAsync(punchItemGuid, category, cancellationToken);
+    }
+}
