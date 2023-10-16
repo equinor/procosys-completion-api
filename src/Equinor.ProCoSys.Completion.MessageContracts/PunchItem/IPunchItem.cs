3fc96ddc1bb71b7e764d9596e6fcbc1826d7e739
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-10-16 14:21:57 +0200

    Extend IPunchItem with all possible properties of PunchItem domain object

--- src/Equinor.ProCoSys.Completion.MessageContracts/PunchItem/IPunchItem.cs
+++ src/Equinor.ProCoSys.Completion.MessageContracts/PunchItem/IPunchItem.cs
@@ -7,11 +7,32 @@ public interface IPunchItem
     Guid ProjectGuid { get; }
     string ProjectName { get; }
     string ProjectDescription { get; }
+    Guid CheckListGuid { get; }
+    string Category { get; }
     int ItemNo { get; }
+    string Description { get; }
+    string RaisedByOrgCode { get; }
+    string ClearingByOrgCode { get; }
+    string? SortingCode { get; }
+    string? TypeCode { get; }
+    string? PriorityCode { get; }
+    DateTime? DueTimeUtc { get; }
+    int? Estimate { get; }
+    string? ExternalItemNo { get; }
+    bool MaterialRequired { get; }
+    DateTime? MaterialETAUtc { get; }
+    string? MaterialExternalNo { get; }
+    string? WorkOrderNo { get; }
+    string? OriginalWorkOrderNo { get; }
+    string? DocumentNo { get; }
+    int? SWCRNo { get; }
+    Guid? ActionByOid { get; }
     Guid? ClearedByOid { get; }
     DateTime? ClearedAtUtc { get; }
     Guid? RejectedByOid { get; }
     DateTime? RejectedAtUtc { get; }
     Guid? VerifiedByOid { get; }
     DateTime? VerifiedAtUtc { get; }
+    Guid CreatedByOid { get; }
+    DateTime CreatedAtUtc { get; }
 }
