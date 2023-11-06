470d22be7c54c1ab93d70f97ec4eb9f92419eaec
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-11-06 13:17:26 +0100

    Rename SourceGuid / SourceType to ParentGuid / ParentType in integration messages for Attachment and Link events. The phrase "Parent" is more generic, and now introduced in Punch where a checklist is parent

--- src/Equinor.ProCoSys.Completion.MessageContracts/LInk/ILink.cs
+++ src/Equinor.ProCoSys.Completion.MessageContracts/LInk/ILink.cs
@@ -4,8 +4,10 @@ namespace Equinor.ProCoSys.Completion.MessageContracts.Link;
 
 public interface ILink
 {
-    Guid SourceGuid { get; }
-    string SourceType { get; }
+    // Guid of the entity owning the Link
+    Guid ParentGuid { get; }
+    // Type of the entity owning the Link
+    string ParentType { get; }
     string Title { get; }
     string Url { get; }
 }
