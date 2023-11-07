e518d80fa16af05777930241a0bb9d0688b68279
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-11-07 09:27:11 +0100

    Migration

---
+++ src/Equinor.ProCoSys.Completion.Infrastructure/Migrations/20231107064234_RenameSourceGuid.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equinor.ProCoSys.Completion.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameSourceGuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SourceType",
+                table: "Links",
+                newName: "ParentType")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "LinksHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "SourceGuid",
+                table: "Links",
+                newName: "ParentGuid")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "LinksHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Links_SourceGuid",
+                table: "Links",
+                newName: "IX_Links_ParentGuid");
+
+            migrationBuilder.RenameColumn(
+                name: "SourceType",
+                table: "Comments",
+                newName: "ParentType")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CommentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "SourceGuid",
+                table: "Comments",
+                newName: "ParentGuid")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CommentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_SourceGuid",
+                table: "Comments",
+                newName: "IX_Comments_ParentGuid");
+
+            migrationBuilder.RenameColumn(
+                name: "SourceType",
+                table: "Attachments",
+                newName: "ParentType")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "AttachmentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "SourceGuid",
+                table: "Attachments",
+                newName: "ParentGuid")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "AttachmentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Attachments_SourceGuid",
+                table: "Attachments",
+                newName: "IX_Attachments_ParentGuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ParentType",
+                table: "Links",
+                newName: "SourceType")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "LinksHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentGuid",
+                table: "Links",
+                newName: "SourceGuid")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "LinksHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Links_ParentGuid",
+                table: "Links",
+                newName: "IX_Links_SourceGuid");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentType",
+                table: "Comments",
+                newName: "SourceType")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CommentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentGuid",
+                table: "Comments",
+                newName: "SourceGuid")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CommentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_ParentGuid",
+                table: "Comments",
+                newName: "IX_Comments_SourceGuid");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentType",
+                table: "Attachments",
+                newName: "SourceType")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "AttachmentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ParentGuid",
+                table: "Attachments",
+                newName: "SourceGuid")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "AttachmentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Attachments_ParentGuid",
+                table: "Attachments",
+                newName: "IX_Attachments_SourceGuid");
+        }
+    }
+}
