ec7a9ad78820782fa3f4c72c7e5a78f395d971ef
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-11-21 12:01:34 +0100

    Unit tests for GetLabelsForHostQueryHandler

--- src/Equinor.ProCoSys.Completion.Infrastructure/CompletionContext.cs
+++ src/Equinor.ProCoSys.Completion.Infrastructure/CompletionContext.cs
@@ -10,6 +10,7 @@ using Equinor.ProCoSys.Completion.Domain.AggregateModels.AttachmentAggregate;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.CommentAggregate;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.DocumentAggregate;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.LabelAggregate;
+using Equinor.ProCoSys.Completion.Domain.AggregateModels.LabelHostAggregate;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.LibraryAggregate;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.LinkAggregate;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.PersonAggregate;
@@ -67,6 +68,7 @@ public class CompletionContext : DbContext, IUnitOfWork, IReadOnlyContext
     public static LibraryTypeConverter LibraryTypeConverter { get; } = new();
 
     public virtual DbSet<Label> Labels => Set<Label>();
+    public virtual DbSet<LabelHost> LabelHosts => Set<LabelHost>();
     public virtual DbSet<Person> Persons => Set<Person>();
     public virtual DbSet<PunchItem> PunchItems => Set<PunchItem>();
     public virtual DbSet<Project> Projects => Set<Project>();
