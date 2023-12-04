16a2d3e96f0093e6614787727f51b2d6904c51f4
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-12-04 11:41:42 +0100

    Added missing tests for DeletePunchItemAttachmentCommandValidator
    (cherry picked from commit aba65d79b1ad55be30a15fc643b0f9a7a152764d)

---
+++ src/tests/Equinor.ProCoSys.Completion.Command.Tests/PunchItemCommands/DeletePunchItemAttachment/DeletePunchItemAttachmentCommandValidatorTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Completion.Command.Attachments;
+using Equinor.ProCoSys.Completion.Command.PunchItemCommands.DeletePunchItemAttachment;
+using Equinor.ProCoSys.Completion.Domain.Validators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Equinor.ProCoSys.Completion.Command.Tests.PunchItemCommands.DeletePunchItemAttachment;
+
+[TestClass]
+public class DeletePunchItemAttachmentCommandValidatorTests
+{
+    private DeletePunchItemAttachmentCommandValidator _dut;
+    private IPunchItemValidator _punchItemValidatorMock;
+    private IAttachmentService _attachmentServiceMock;
+    private DeletePunchItemAttachmentCommand _command;
+
+    [TestInitialize]
+    public void Setup_OkState()
+    {
+        _command = new DeletePunchItemAttachmentCommand(Guid.NewGuid(), Guid.NewGuid(), "r");
+        _punchItemValidatorMock = Substitute.For<IPunchItemValidator>();
+        _punchItemValidatorMock.ExistsAsync(_command.PunchItemGuid, default)
+            .Returns(true);
+        _attachmentServiceMock = Substitute.For<IAttachmentService>();
+        _attachmentServiceMock.ExistsAsync(_command.AttachmentGuid, default)
+            .Returns(true);
+        _dut = new DeletePunchItemAttachmentCommandValidator(
+            _punchItemValidatorMock,
+            _attachmentServiceMock);
+    }
+
+    [TestMethod]
+    public async Task Validate_ShouldBeValid_WhenOkState()
+    {
+        var result = await _dut.ValidateAsync(_command);
+
+        Assert.IsTrue(result.IsValid);
+    }
+
+    [TestMethod]
+    public async Task Validate_ShouldFail_When_PunchItemNotExists()
+    {
+        // Arrange
+        _punchItemValidatorMock.ExistsAsync(_command.PunchItemGuid, default)
+            .Returns(false);
+
+        // Act
+        var result = await _dut.ValidateAsync(_command);
+
+        // Assert
+        Assert.IsFalse(result.IsValid);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Punch item with this guid does not exist!"));
+    }
+
+    [TestMethod]
+    public async Task Validate_ShouldFail_When_AttachmentNotExists()
+    {
+        // Arrange
+        _attachmentServiceMock.ExistsAsync(_command.AttachmentGuid, default)
+            .Returns(false);
+
+        // Act
+        var result = await _dut.ValidateAsync(_command);
+
+        // Assert
+        Assert.IsFalse(result.IsValid);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Attachment with this guid does not exist!"));
+    }
+
+    [TestMethod]
+    public async Task Validate_ShouldFail_When_TagOwningPunchItemIsVoided()
+    {
+        // Arrange
+        _punchItemValidatorMock.TagOwningPunchItemIsVoidedAsync(_command.PunchItemGuid, default)
+            .Returns(true);
+
+        // Act
+        var result = await _dut.ValidateAsync(_command);
+
+        // Assert
+        Assert.IsFalse(result.IsValid);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Tag owning punch item is voided!"));
+    }
+
+    [TestMethod]
+    public async Task Validate_ShouldFail_When_ProjectIsClosed()
+    {
+        // Arrange
+        _punchItemValidatorMock.ProjectOwningPunchItemIsClosedAsync(_command.PunchItemGuid, default)
+            .Returns(true);
+
+        // Act
+        var result = await _dut.ValidateAsync(_command);
+
+        // Assert
+        Assert.IsFalse(result.IsValid);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Project is closed!"));
+    }
+
+    [TestMethod]
+    public async Task Validate_ShouldFail_When_PunchItemIsVerified()
+    {
+        // Arrange
+        _punchItemValidatorMock.IsVerifiedAsync(_command.PunchItemGuid, default)
+            .Returns(true);
+
+        // Act
+        var result = await _dut.ValidateAsync(_command);
+
+        // Assert
+        Assert.IsFalse(result.IsValid);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Punch item attachments can't be changed. The punch item is verified!"));
+    }
+}
