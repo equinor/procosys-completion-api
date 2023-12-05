c17a6e01205771b755d65f63f15d910a5e80886e
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-12-05 08:39:37 +0100

    AccessValidatorForUpdatePunchItemAttachmentCommandTests
    (cherry picked from commit 93b53b13f603f8b410da2341957addaa846e959d)

---
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.Tests/Authorizations/IsPunchItemCommandTests/AccessValidatorForUpdatePunchItemAttachmentCommandTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Equinor.ProCoSys.Completion.Command.PunchItemCommands.UpdatePunchItemAttachment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.Completion.WebApi.Tests.Authorizations.IsPunchItemCommandTests;
+
+[TestClass]
+public class AccessValidatorForUpdatePunchItemAttachmentCommandTests : AccessValidatorForIIsPunchItemCommandTests<UpdatePunchItemAttachmentCommand>
+{
+    protected override UpdatePunchItemAttachmentCommand GetPunchItemCommandWithAccessToBothProjectAndContent()
+        => new(PunchItemGuidWithAccessToProjectAndContent, Guid.Empty, "a", null!, null!);
+
+    protected override UpdatePunchItemAttachmentCommand GetPunchItemCommandWithAccessToProjectButNotContent()
+        => new(PunchItemGuidWithAccessToProjectButNotContent, Guid.Empty, "a", null!, null!);
+
+    protected override UpdatePunchItemAttachmentCommand GetPunchItemCommandWithoutAccessToProject()
+        => new(PunchItemGuidWithoutAccessToProject, Guid.Empty, "a", null!, null!);
+}
