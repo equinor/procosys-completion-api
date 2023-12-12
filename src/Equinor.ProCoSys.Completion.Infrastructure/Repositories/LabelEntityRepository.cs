c38590428aa9a67158d58f8b7eaef7de215d7f6e
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-12-12 07:43:30 +0100

    LabelEntityRepositoryTests

--- src/Equinor.ProCoSys.Completion.Infrastructure/Repositories/LabelEntityRepository.cs
+++ src/Equinor.ProCoSys.Completion.Infrastructure/Repositories/LabelEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.LabelEntityAggregate;
@@ -13,8 +14,17 @@ public class LabelEntityRepository : EntityRepository<LabelEntity>, ILabelEntity
     {
     }
 
-    public Task<LabelEntity> GetByTypeAsync(EntityTypeWithLabel entityType, CancellationToken cancellationToken)
-        => DefaultQuery.Where(e => e.EntityType == entityType).SingleAsync(cancellationToken);
+    public async Task<LabelEntity> GetByTypeAsync(EntityTypeWithLabel entityType, CancellationToken cancellationToken)
+    {
+        var labelEntity = await DefaultQuery.Where(e => e.EntityType == entityType)
+            .SingleOrDefaultAsync(cancellationToken);
+        if (labelEntity == null)
+        {
+            throw new Exception($"Label entity {entityType} not found");
+        }
+
+        return labelEntity;
+    }
 
     public async Task<bool> ExistsAsync(EntityTypeWithLabel entityType, CancellationToken cancellationToken)
         => await DefaultQuery.AnyAsync(e => e.EntityType == entityType, cancellationToken);
