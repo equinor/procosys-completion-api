8d48c37661ec6ca897fc0893d95dfedaad70f37b
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-12-15 07:40:09 +0100

    Added integration tests for LabelsController

---
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.IntegrationTests/Labels/LabelsControllerNegativeTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Completion.Domain.AggregateModels.LabelEntityAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.Completion.WebApi.IntegrationTests.Labels;
+
+[TestClass]
+public class LabelsControllerNegativeTests : TestBase
+{
+    #region GetLabels
+    [TestMethod]
+    public async Task GetLabels_AsAnonymous_ShouldReturnUnauthorized()
+        => await LabelsControllerTestsHelper.GetLabelsAsync(
+            UserType.Anonymous,
+            HttpStatusCode.Unauthorized);
+
+    // Todo 108512 Enable test after secured with superuser permission
+    //[TestMethod]
+    //public async Task GetLabels_AsReader_ShouldReturnForbidden()
+    //    => await LabelsControllerTestsHelper.GetLabelsAsync(
+    //        UserType.Reader,
+    //        HttpStatusCode.Forbidden);
+    #endregion
+
+    #region CreateLabel
+    [TestMethod]
+    public async Task CreateLabel_AsAnonymous_ShouldReturnUnauthorized()
+        => await LabelsControllerTestsHelper.CreateLabelAsync(
+            UserType.Anonymous,
+            Guid.NewGuid().ToString(),
+            HttpStatusCode.Unauthorized);
+
+    // Todo 108512 Enable test after secured with superuser permission
+    //[TestMethod]
+    //public async Task CreateLabel_AsReader_ShouldReturnForbidden()
+    //    => await LabelsControllerTestsHelper.CreateLabelAsync(
+    //        UserType.Reader,
+    //        Guid.NewGuid().ToString(),
+    //        HttpStatusCode.Forbidden);
+    #endregion
+
+    #region UpdateLabel
+    [TestMethod]
+    public async Task UpdateLabel_AsAnonymous_ShouldReturnUnauthorized()
+        => await LabelsControllerTestsHelper.UpdateLabelAsync(
+            UserType.Anonymous,
+            Guid.NewGuid().ToString(),
+            new List<EntityTypeWithLabel>(),
+            HttpStatusCode.Unauthorized);
+
+    // Todo 108512 Enable test after secured with superuser permission
+    //[TestMethod]
+    //public async Task UpdateLabel_AsReader_ShouldReturnForbidden()
+    //{
+    //    // Arrange
+    //    var text = Guid.NewGuid().ToString();
+    //    await LabelsControllerTestsHelper.CreateLabelAsync(UserType.Writer, text);
+
+    //    // Act and Assert
+    //    await LabelsControllerTestsHelper.UpdateLabelAsync(
+    //        UserType.Reader,
+    //        text,
+    //        new List<EntityTypeWithLabel>(),
+    //        HttpStatusCode.Forbidden);
+    //}
+
+    #endregion
+}
---
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.IntegrationTests/Labels/LabelsControllerTestsHelper.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Completion.Domain.AggregateModels.LabelEntityAggregate;
+using Newtonsoft.Json;
+
+namespace Equinor.ProCoSys.Completion.WebApi.IntegrationTests.Labels;
+
+public static class LabelsControllerTestsHelper
+{
+    private const string Route = "Labels";
+
+    public static async Task<List<LabelDto>> GetLabelsAsync(
+        UserType userType,
+        HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+        string expectedMessageOnBadRequest = null)
+    {
+        var response = await TestFactory.Instance.GetHttpClient(userType, null).GetAsync(Route);
+
+        await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
+
+        if (expectedStatusCode != HttpStatusCode.OK)
+        {
+            return null;
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<List<LabelDto>>(content);
+    }
+
+    public static async Task<string> CreateLabelAsync(
+        UserType userType,
+        string text,
+        HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+        string expectedMessageOnBadRequest = null)
+    {
+        var bodyPayload = new
+        {
+            text
+        };
+
+        var serializePayload = JsonConvert.SerializeObject(bodyPayload);
+        var content = new StringContent(serializePayload, Encoding.UTF8, "application/json");
+        var response = await TestFactory.Instance.GetHttpClient(userType, null).PostAsync(Route, content);
+        await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            return null;
+        }
+
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    public static async Task UpdateLabelAsync(
+        UserType userType,
+        string text,
+        List<EntityTypeWithLabel> availableForLabels,
+        HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+        string expectedMessageOnBadRequest = null)
+    {
+        var bodyPayload = new
+        {
+            text,
+            availableForLabels
+        };
+
+        var serializePayload = JsonConvert.SerializeObject(bodyPayload);
+        var content = new StringContent(serializePayload, Encoding.UTF8, "application/json");
+        var response = await TestFactory.Instance.GetHttpClient(userType, null).PutAsync(Route, content);
+        await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
+    }
+}
