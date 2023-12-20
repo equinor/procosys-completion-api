179114cc09c043f631733648036c8faca7a176cf
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-12-20 07:19:19 +0100

    Prohibit add link after clear

--- src/tests/Equinor.ProCoSys.Completion.Command.Tests/PunchItemCommands/CreatePunchItemLink/CreatePunchItemLinkCommandValidatorTests.cs
+++ src/tests/Equinor.ProCoSys.Completion.Command.Tests/PunchItemCommands/CreatePunchItemLink/CreatePunchItemLinkCommandValidatorTests.cs
@@ -78,4 +78,20 @@ public class CreatePunchItemLinkCommandValidatorTests
         Assert.AreEqual(1, result.Errors.Count);
         Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Project is closed!"));
     }
+
+    [TestMethod]
+    public async Task Validate_ShouldFail_When_PunchItemIsCleared()
+    {
+        // Arrange
+        _punchItemValidatorMock.IsClearedAsync(_command.PunchItemGuid, default)
+            .Returns(true);
+
+        // Act
+        var result = await _dut.ValidateAsync(_command);
+
+        // Assert
+        Assert.IsFalse(result.IsValid);
+        Assert.AreEqual(1, result.Errors.Count);
+        Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Punch item is cleared!"));
+    }
 }
