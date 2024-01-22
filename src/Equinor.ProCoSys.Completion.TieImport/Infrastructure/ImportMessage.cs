3607c0d00c49ca5ae5d25c79cd3082cb4d47de32
Jostein Søiland <jsoi@equinor.com>
2024-01-22 09:13:34 +0100

    Remove ConfigurationHelper, IMessageInspector, ImportMessage

--- src/Equinor.ProCoSys.Completion.TieImport/Infrastructure/ImportMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Equinor.ProCoSys.Completion.TieImport.Infrastructure;
-public sealed class ImportMessage
-{
-    public string Message { get; set; } = "";
-
-    public string? Details { get; set; } = "";
-}
