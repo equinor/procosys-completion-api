98a27e720ae6e03625e33e63c83eb5473b8feeaf
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-01-24 13:34:51 +0100

    Doc of mail template with placeholders and use of dynamic content

--- src/Equinor.ProCoSys.Completion.Command/Email/ICompletionMailService.cs
+++ src/Equinor.ProCoSys.Completion.Command/Email/ICompletionMailService.cs
@@ -6,5 +6,25 @@ namespace Equinor.ProCoSys.Completion.Command.Email;
 
 public interface ICompletionMailService
 {
+    /*
+        Sample of how to build dynamic content to be used to replace placeholders in a mail template
+
+        string status = "Your order is sent";
+        MyPerson person = new MyPerson { Name = "John Doe" };
+        MyOrder order = new MyOrder {
+            Person = person,
+            Id = 101
+        }
+
+        dynamic context = new ExpandoObject();
+        context.Order = order;
+        context.Status = status;
+
+        Sample mail template with 3 placeholders:
+        string mailTemplate = "Hi {{MyClass.Person.Name}}, status of your order {{Order.Id}} is '{{Status}}'."
+    
+        ITemplateTransformer.Transform(mailTemplate, context) will return:
+        "Hi John Doe, status of your order 101 is 'Your order is sent'."
+    */
     Task SendEmailAsync(dynamic context, string eMailCode, List<string> eMailAddresses, CancellationToken cancellationToken);
 }
--- src/Equinor.ProCoSys.Completion.Domain/EmailTemplateCode.cs
+++ src/Equinor.ProCoSys.Completion.Domain/EmailTemplateCode.cs
@@ -3,6 +3,6 @@
 public static class EmailTemplateCode
 {
     // keep these constants in alphabetic order
-    public const string PunchComment = "PUNCH_COMMENT";
+    public const string PunchCommented = "PUNCH_COMMENTED";
     public const string PunchRejected = "PUNCH_REJECTED";
 }
