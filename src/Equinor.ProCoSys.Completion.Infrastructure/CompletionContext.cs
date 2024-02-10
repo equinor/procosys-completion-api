7a4e47ba3ede7e7e7882b5f490843eabb81d96b4
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2024-02-10 11:07:18 +0100

    Update CompletionContext.cs
    parameterless constructor

--- src/Equinor.ProCoSys.Completion.Infrastructure/CompletionContext.cs
+++ src/Equinor.ProCoSys.Completion.Infrastructure/CompletionContext.cs
@@ -35,6 +35,13 @@ public class CompletionContext : DbContext, IUnitOfWork, IReadOnlyContext
     private readonly IEventDispatcher _eventDispatcher;
     private readonly ICurrentUserProvider _currentUserProvider;
 
+    // The design-time tools used by the Entity Framework creating migrations 
+    // need parameterless constructor be able to create 
+    // an instance of the context without parameters.
+    public CompletionContext()
+    {
+    }
+
     public CompletionContext(
         DbContextOptions<CompletionContext> options,
         IPlantProvider plantProvider,
