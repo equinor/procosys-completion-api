2cec527d85d4595c8286e639e645bcf966ebdd12
Espen Hatleskog <EHA@equinor.com>
2024-03-21 13:46:26 +0100

    feat: add endpoint for getting persons from main web api

---
+++ src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/Persons/IPersonApiService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Equinor.ProCoSys.Completion.ForeignApi.MainApi.Persons;
+
+public interface IPersonApiService
+{
+    Task<List<ProCoSys4Person>> GetAllPersonsAsync(string plant, CancellationToken cancellationToken);
+}
---
+++ src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/Persons/IPersonCache.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Equinor.ProCoSys.Completion.ForeignApi.MainApi.Persons;
+
+public interface IPersonCache
+{
+    Task<List<ProCoSys4Person>> GetAllPersonsAsync(string plant, CancellationToken cancellationToken);
+}
