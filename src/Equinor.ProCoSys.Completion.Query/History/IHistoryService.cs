ead1270470706c0fa4e218f2537cae35d5506127
trondlindanger <66942962+trondlindanger@users.noreply.github.com>
2024-05-27 14:41:41 +0200

    Add history get all (#145)
    * Implementation of dummy GetAll endpoint for history.
    
    * Retrieve history from database.
    
    * Remove unneccessary import.
    
    * Adjustments related to PR.

---
+++ src/Equinor.ProCoSys.Completion.Query/History/IHistoryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Equinor.ProCoSys.Completion.Query.History;
+
+public interface IHistoryService
+{
+    Task<IEnumerable<HistoryDto>> GetAllAsync(
+        Guid parentGuid,
+        CancellationToken cancellationToken);
+}
---
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.Tests/Authorizations/IsPunchItemQueryTests/AccessValidatorForGetPunchItemHistoryQueryTests.cs
@@ -0,0 +1,14 @@
+﻿using Equinor.ProCoSys.Completion.Query.PunchItemQueries.GetPunchItemHistory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.Completion.WebApi.Tests.Authorizations.IsPunchItemQueryTests;
+
+[TestClass]
+public class AccessValidatorForGetPunchItemHistoryQueryTests : AccessValidatorForIIsPunchItemQueryTests<GetPunchItemHistoryQuery>
+{
+    protected override GetPunchItemHistoryQuery GetPunchItemQueryWithAccessToProject()
+        => new(PunchItemGuidWithAccessToProjectAndContent);
+
+    protected override GetPunchItemHistoryQuery GetPunchItemQueryWithoutAccessToProject()
+        => new(PunchItemGuidWithoutAccessToProject);
+}
