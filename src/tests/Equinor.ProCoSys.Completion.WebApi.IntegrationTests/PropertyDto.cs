8d55b8cc1d6f39141fd2cc3df23d02884713f45c
trondlindanger <66942962+trondlindanger@users.noreply.github.com>
2024-05-30 09:06:12 +0200

    History get all integration (#150)
    * Integration test for GetHistory.
    
    * Handled merge conflict.

---
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.IntegrationTests/PropertyDto.cs
@@ -0,0 +1,8 @@
+﻿#nullable enable
+namespace Equinor.ProCoSys.Completion.WebApi.IntegrationTests;
+
+public record PropertyDto(
+    string Name,
+    string? OldValue,
+    string? Value,
+    string ValueDisplayType);
