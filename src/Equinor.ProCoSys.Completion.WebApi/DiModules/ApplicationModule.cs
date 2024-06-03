09ee48f8ea9270192012975beaaefccf5e75287e
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-06-03 13:35:20 +0200

    Added ns

--- src/Equinor.ProCoSys.Completion.WebApi/DiModules/ApplicationModule.cs
+++ src/Equinor.ProCoSys.Completion.WebApi/DiModules/ApplicationModule.cs
@@ -37,6 +37,7 @@ using Equinor.ProCoSys.Completion.WebApi.Authentication;
 using Equinor.ProCoSys.Completion.WebApi.Authorizations;
 using Equinor.ProCoSys.Completion.WebApi.Controllers;
 using Equinor.ProCoSys.Completion.WebApi.Misc;
+using Equinor.ProCoSys.Completion.WebApi.Synchronization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
