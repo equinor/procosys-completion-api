01afdab03ad361cb5b7dc38fc083996b7d18b980
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-06-03 14:39:31 +0200

    Fix after merge. Improve test

--- src/tests/Equinor.ProCoSys.Completion.Query.Tests/History/HistoryServiceTests.cs
+++ src/tests/Equinor.ProCoSys.Completion.Query.Tests/History/HistoryServiceTests.cs
@@ -1,25 +1,20 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Equinor.ProCoSys.Completion.Domain.AggregateModels.CommentAggregate;
-using Equinor.ProCoSys.Completion.Domain.AggregateModels.LabelAggregate;
-using Equinor.ProCoSys.Completion.Test.Common;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Equinor.ProCoSys.Completion.Infrastructure;
-using Microsoft.EntityFrameworkCore;
-using Equinor.ProCoSys.Completion.Query.Comments;
-using System.Collections.Generic;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.HistoryAggregate;
-using Equinor.ProCoSys.Completion.Domain.AggregateModels.PersonAggregate;
-using Equinor.ProCoSys.Completion.MessageContracts.History;
+using Equinor.ProCoSys.Completion.Infrastructure;
 using Equinor.ProCoSys.Completion.Query.History;
+using Equinor.ProCoSys.Completion.Test.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Equinor.ProCoSys.Completion.Query.Tests.History;
 
 [TestClass]
 public class HistoryServiceTests : ReadOnlyTestsBase
 {
-    private HistoryItem _createdHistoryItem;
+    private HistoryItem _oldestHistoryItem;
+    private HistoryItem _newestHistoryItem;
     private Guid _eventForGuid;
 
     protected override void SetupNewDatabase(DbContextOptions<CompletionContext> dbContextOptions)
@@ -27,16 +22,31 @@ public class HistoryServiceTests : ReadOnlyTestsBase
         using var context = new CompletionContext(dbContextOptions, _plantProviderMock, _eventDispatcherMock, _currentUserProviderMock, _tokenCredentialsMock);
 
         _eventForGuid = Guid.NewGuid();
-        var eventByOid = CurrentUserOid;
         var eventAtUtc = DateTime.UtcNow;
-        _createdHistoryItem = new HistoryItem(_eventForGuid, "eventDisplayName", eventByOid, "eventByFullName", eventAtUtc);
-        _createdHistoryItem.AddPropertyForCreate("propertyName","propertyValue",ValueDisplayType.StringAsText);
-        context.History.Add(_createdHistoryItem);
+        _oldestHistoryItem = new HistoryItem(
+            _eventForGuid, 
+            "eventDisplayName1", 
+            Guid.NewGuid(), 
+            "eventByFullName1", 
+            eventAtUtc.AddSeconds(1), 
+            Guid.NewGuid());
+        var property1 = new Property("P1", "D1");
+        var property2 = new Property("P2", "D2");
+        _oldestHistoryItem.AddProperty(property1);
+        _oldestHistoryItem.AddProperty(property2);
+        _newestHistoryItem = new HistoryItem(
+            _eventForGuid, 
+            "eventDisplayName2", 
+            Guid.NewGuid(), 
+            "eventByFullName2", 
+            eventAtUtc.AddSeconds(2));
+        context.History.Add(_oldestHistoryItem);
+        context.History.Add(_newestHistoryItem);
         context.SaveChangesAsync().Wait();
     }
 
     [TestMethod]
-    public async Task GetAllAsync_ShouldReturnCorrectDtos()
+    public async Task GetAllAsync_ShouldReturnCorrectDtosOrderedDescending()
     {
         // Arrange
         await using var context = new CompletionContext(_dbContextOptions, _plantProviderMock, _eventDispatcherMock, _currentUserProviderMock, _tokenCredentialsMock);
@@ -47,22 +57,28 @@ public class HistoryServiceTests : ReadOnlyTestsBase
 
         // Assert
         var historyDtos = result.ToList();
-        Assert.AreEqual(1, historyDtos.Count);
-        var historyDto = historyDtos.ElementAt(0);
-        Assert.AreEqual(_createdHistoryItem.EventForGuid, historyDto.EventForGuid);
-        Assert.AreEqual(_createdHistoryItem.EventAtUtc, historyDto.EventAtUtc);
-        Assert.AreEqual(_createdHistoryItem.EventByFullName, historyDto.EventByFullName);
-        Assert.AreEqual(CurrentUserOid, historyDto.EventByOid);
-        AssertProperties(_createdHistoryItem, historyDto);
+        Assert.AreEqual(2, historyDtos.Count);
+        AssertHistory(_newestHistoryItem, historyDtos.ElementAt(0));
+        AssertHistory(_oldestHistoryItem, historyDtos.ElementAt(1));
     }
 
-    private static void AssertProperties(HistoryItem history, HistoryDto historyDto)
+    private void AssertHistory(HistoryItem historyItem, HistoryDto historyDto)
+    {
+        Assert.AreEqual(historyItem.EventForGuid, historyDto.EventForGuid);
+        Assert.AreEqual(historyItem.EventAtUtc, historyDto.EventAtUtc);
+        Assert.AreEqual(historyItem.EventByFullName, historyDto.EventByFullName);
+        Assert.AreEqual(historyItem.EventByOid, historyDto.EventByOid);
+        AssertProperties(historyItem, historyDto);
+
+    }
+
+    private static void AssertProperties(HistoryItem historyItem, HistoryDto historyDto)
     {
         Assert.IsNotNull(historyDto.Properties);
-        Assert.AreEqual(history.Properties.Count, historyDto.Properties.Count);
-        for (var i = 0; i < history.Properties.Count; i++)
+        Assert.AreEqual(historyItem.Properties.Count, historyDto.Properties.Count);
+        for (var i = 0; i < historyItem.Properties.Count; i++)
         {
-            var expectedProperty = history.Properties.ElementAt(i);
+            var expectedProperty = historyItem.Properties.ElementAt(i);
             var property = historyDto.Properties.ElementAt(i);
             Assert.AreEqual(expectedProperty.Name, property.Name);
             Assert.AreEqual(expectedProperty.Value, property.Value);
