74b1f2fa62c880aeda32c0b90bdb0cbadb272e1b
Espen Hatleskog <eha@equinor.com>
2024-06-12 14:04:30 +0200

    Test for delete punchitem in consumer (#174)
    * impl test for delete and fixed test after refactor
    
    * missed a few places
    
    * removed abstract for query and document consumer

--- src/tests/Equinor.ProCoSys.Completion.WebApi.Tests/Synchronization/LibraryEventConsumerTests.cs
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.Tests/Synchronization/LibraryEventConsumerTests.cs
@@ -64,7 +64,7 @@ public class LibraryEventConsumerTests
         Assert.AreEqual(Description,_libraryAddedToRepository.Description);
         Assert.AreEqual(Code,_libraryAddedToRepository.Code);
         Assert.AreEqual(Type, _libraryAddedToRepository.Type);
-        await _unitOfWorkMock.Received(1).SaveChangesAsync();
+        await _unitOfWorkMock.Received(1).SaveChangesFromSyncAsync();
     }
     
     [TestMethod]
@@ -94,7 +94,7 @@ public class LibraryEventConsumerTests
         Assert.AreEqual(Description,libraryItemToUpdate.Description);
         Assert.AreEqual(Code,libraryItemToUpdate.Code);
         Assert.AreEqual(Type, libraryItemToUpdate.Type);
-        await _unitOfWorkMock.Received(1).SaveChangesAsync();
+        await _unitOfWorkMock.Received(1).SaveChangesFromSyncAsync();
     }
     
     [TestMethod]
