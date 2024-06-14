bbdbc333cdd86aeedad5dc28a98c38ff8886319c
agustarnason <aarna@equinor.com>
2024-06-14 10:09:58 +0200

    Undid the caching for Sync Token, as Microsoft Identity automatically caches the token locally until it expires

--- src/Equinor.ProCoSys.Completion.DbSyncToPCS4/Service/SyncBearerTokenHandler.cs
+++ src/Equinor.ProCoSys.Completion.DbSyncToPCS4/Service/SyncBearerTokenHandler.cs
@@ -1,31 +1,19 @@
 ﻿using System.Net.Http.Headers;
-using Equinor.ProCoSys.Common.Caches;
 
 namespace Equinor.ProCoSys.Completion.DbSyncToPCS4.Service;
 
 public class SyncBearerTokenHandler : DelegatingHandler
 {
     private readonly ISyncTokenService _syncTokenService;
-    private readonly ICacheManager _cacheManager;
-    private readonly int _syncTokenCacheInMinutes = 60;
 
-    public SyncBearerTokenHandler(ISyncTokenService syncTokenService, ICacheManager cacheManager)
+    public SyncBearerTokenHandler(ISyncTokenService syncTokenService)
     {
         _syncTokenService = syncTokenService;
-        _cacheManager = cacheManager;
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var token = await _cacheManager.GetOrCreate(
-            "SYNC_TOKEN",
-            async () =>
-            {
-                var checkList = await _syncTokenService.AquireTokenAsync();
-                return checkList;
-            },
-            CacheDuration.Minutes,
-            _syncTokenCacheInMinutes);
+        var token = await _syncTokenService.AquireTokenAsync();
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
         return await base.SendAsync(request, cancellationToken);
     }
