e5c5c0acbb7d7049a9e149810e28adc1c29d48fa
agustarnason <aarna@equinor.com>
2024-06-14 08:53:59 +0200

    Added caching for Sync Token

--- src/Equinor.ProCoSys.Completion.DbSyncToPCS4/Service/SyncBearerTokenHandler.cs
+++ src/Equinor.ProCoSys.Completion.DbSyncToPCS4/Service/SyncBearerTokenHandler.cs
@@ -1,19 +1,31 @@
 ﻿using System.Net.Http.Headers;
+using Equinor.ProCoSys.Common.Caches;
 
 namespace Equinor.ProCoSys.Completion.DbSyncToPCS4.Service;
 
 public class SyncBearerTokenHandler : DelegatingHandler
 {
     private readonly ISyncTokenService _syncTokenService;
+    private readonly ICacheManager _cacheManager;
+    private readonly int _syncTokenCacheInMinutes = 60;
 
-    public SyncBearerTokenHandler(ISyncTokenService syncTokenService)
+    public SyncBearerTokenHandler(ISyncTokenService syncTokenService, ICacheManager cacheManager)
     {
         _syncTokenService = syncTokenService;
+        _cacheManager = cacheManager;
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var token = await _syncTokenService.AquireTokenAsync();
+        var token = await _cacheManager.GetOrCreate(
+            "SYNC_TOKEN",
+            async () =>
+            {
+                var checkList = await _syncTokenService.AquireTokenAsync();
+                return checkList;
+            },
+            CacheDuration.Minutes,
+            _syncTokenCacheInMinutes);
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
         return await base.SendAsync(request, cancellationToken);
     }
