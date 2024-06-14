065efd2903d30da9227b02ca46e0e28ee63940e6
tordj <tordj@equinor.com>
2024-06-14 14:07:35 +0200

    revert code and script for setting unknown lib value.
    also add check that raisedBy and ClearingBy has value when they reach consumer

---
+++ src/Equinor.ProCoSys.Completion.Infrastructure/Migrations/20240614120621_RemovSeededLibValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equinor.ProCoSys.Completion.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovSeededLibValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Library",
+                keyColumn: "Id",
+                keyValue: -1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Library",
+                columns: new[] { "Id", "Code", "Description", "Guid", "IsVoided", "Plant", "ProCoSys4LastUpdated", "SyncTimestamp", "Type" },
+                values: new object[] { -1, "UNKNOWN", "Null value in oracle db", new Guid("dbd52718-a64e-45a8-b1c5-8779d6c7170b"), true, "N/A", new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "COMPLETION_ORGANIZATION" });
+        }
+    }
+}
