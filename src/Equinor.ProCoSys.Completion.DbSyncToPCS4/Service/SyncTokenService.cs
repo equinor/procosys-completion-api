968c2fcc067fcd1aacec2e9e7ec66bf8f0658577
Sindre Knudsen Smistad <sks@equinor.com>
2024-06-28 07:39:35 +0200

    #196 added cache for db sync tokens (#197)
    * move work out of transactions
    
    * Fix PR comments
    
    * added redis cache
    
    * fix test
    
    * Add config option for checklist expiration
    
    * use IOptionsMonitor instead of IOptionsSnapshot
    
    * added token cache for db sync

--- src/Equinor.ProCoSys.Completion.DbSyncToPCS4/Service/SyncTokenService.cs
+++ src/Equinor.ProCoSys.Completion.DbSyncToPCS4/Service/SyncTokenService.cs
@@ -1,33 +1,19 @@
 ﻿using Microsoft.Extensions.Options;
 using Microsoft.Identity.Client;
 using Equinor.ProCoSys.Auth.Authentication;
+using Microsoft.Identity.Web;
 
 namespace Equinor.ProCoSys.Completion.DbSyncToPCS4.Service;
 
-public class SyncTokenService : ISyncTokenService
+public class SyncTokenService(IOptionsMonitor<SyncToPCS4Options> options, ITokenAcquisition tokenAcquisition)
+    : ISyncTokenService
 {
-    private readonly IConfidentialClientApplication _app;
-    private readonly IAuthenticatorOptions _authenticatorOptions;
-    private readonly IOptionsMonitor<SyncToPCS4Options> _options;
-
-    public SyncTokenService(IAuthenticatorOptions authenticatorOptions, IOptionsMonitor<SyncToPCS4Options> options)
-    {
-        _authenticatorOptions = authenticatorOptions;
-        _options = options;
-        _app = ConfidentialClientApplicationBuilder.Create(_authenticatorOptions.ClientId)
-            .WithClientSecret(_authenticatorOptions.ClientSecret)
-            .WithAuthority(new Uri($"{_authenticatorOptions.Authority}"))
-            .Build();
-    }
-
     public async Task<string> AquireTokenAsync()
     {
         try
         {
-            var optionValues = _options.CurrentValue;
-            var result = await _app.AcquireTokenForClient([optionValues.Scope]).ExecuteAsync();
-
-            return result.AccessToken;
+            var appToken = await tokenAcquisition.GetAccessTokenForAppAsync(options.CurrentValue.Scope);
+            return appToken;
         }
         catch (Exception ex) {
             throw new Exception("There was a problem fetching the PCS4 Database Sync Bearer Token", ex);
