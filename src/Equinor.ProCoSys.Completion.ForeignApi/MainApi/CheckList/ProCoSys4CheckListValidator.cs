cb1858f4dda165243deab0434e69984c6f53e540
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-08-08 10:49:18 +0200

    Do not pass plant to the GET endpoint for checklist in Main API due to performance. The endpoint has m2m auth, hence it doesn't require plant specific permissions

--- src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/CheckList/ProCoSys4CheckListValidator.cs
+++ src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/CheckList/ProCoSys4CheckListValidator.cs
@@ -1,31 +1,30 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Equinor.ProCoSys.Common.Misc;
 using Equinor.ProCoSys.Completion.Domain.Validators;
 
 namespace Equinor.ProCoSys.Completion.ForeignApi.MainApi.CheckList;
 
 // This class is temporary while Pcs4 is master of CheckLists.
 // The class will probably die when Pcs5 become master of CheckLists 
-public class ProCoSys4CheckListValidator(ICheckListCache checkListCache, IPlantProvider plantProvider) : ICheckListValidator
+public class ProCoSys4CheckListValidator(ICheckListCache checkListCache) : ICheckListValidator
 {
     public async Task<bool> ExistsAsync(Guid checkListGuid, CancellationToken cancellationToken)
     {
-        var proCoSys4CheckList = await checkListCache.GetCheckListAsync(plantProvider.Plant, checkListGuid, cancellationToken);
+        var proCoSys4CheckList = await checkListCache.GetCheckListAsync(checkListGuid, cancellationToken);
         return proCoSys4CheckList is not null;
     }
 
     public async Task<bool> TagOwningCheckListIsVoidedAsync(Guid checkListGuid,
         CancellationToken cancellationToken)
     {
-        var proCoSys4CheckList = await checkListCache.GetCheckListAsync(plantProvider.Plant, checkListGuid, cancellationToken);
+        var proCoSys4CheckList = await checkListCache.GetCheckListAsync(checkListGuid, cancellationToken);
         return proCoSys4CheckList is not null && proCoSys4CheckList.IsVoided;
     }
 
     public async Task<bool> InProjectAsync(Guid checkListGuid, Guid projectGuid, CancellationToken cancellationToken)
     {
-        var proCoSys4CheckList = await checkListCache.GetCheckListAsync(plantProvider.Plant, checkListGuid, cancellationToken);
+        var proCoSys4CheckList = await checkListCache.GetCheckListAsync(checkListGuid, cancellationToken);
         return proCoSys4CheckList is not null && proCoSys4CheckList.ProjectGuid == projectGuid;
     }
 }
