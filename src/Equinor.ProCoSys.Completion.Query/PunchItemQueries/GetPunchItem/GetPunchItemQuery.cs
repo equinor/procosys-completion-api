eb0aa4c8111e024958e0c9f45a0bcded85abb540
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-08-15 08:41:14 +0200

    GetPunchItemQuery now inherit NeedProjectAccess and implements IIsPunchItemQuery. This cause that the punch dto is prefetched and access is checked in MediatR pipeline

---
+++ src/Equinor.ProCoSys.Completion.Query/PunchItemQueries/GetPunchItem/GetPunchItemQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Equinor.ProCoSys.Completion.Domain;
+using Equinor.ProCoSys.Completion.Query.PunchItemServices;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.ProCoSys.Completion.Query.PunchItemQueries.GetPunchItem;
+
+public class GetPunchItemQuery(Guid punchItemGuid)
+    : NeedProjectAccess, IRequest<Result<PunchItemDetailsDto>>, IIsPunchItemQuery
+{
+    public Guid PunchItemGuid { get; } = punchItemGuid;
+    public PunchItemDetailsDto PunchItemDetailsDto { get; set; } = null!;
+    public override Guid GetProjectGuidForAccessCheck() => PunchItemDetailsDto.ProjectGuid;
+}
