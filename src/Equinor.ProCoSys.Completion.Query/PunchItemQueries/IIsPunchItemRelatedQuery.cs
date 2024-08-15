ebc0bc6aec23fc84f8f00a4b4d4bde20790d0853
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-08-15 08:38:58 +0200

    Made interface IIsPunchItemRelatedQuery interface with ProjectDetailsDto. ProjectDetailsDto set in PrefetchBehaviour

---
+++ src/Equinor.ProCoSys.Completion.Query/PunchItemQueries/IIsPunchItemRelatedQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Equinor.ProCoSys.Completion.Query.PunchItemServices;
+using MediatR;
+
+namespace Equinor.ProCoSys.Completion.Query.PunchItemQueries;
+
+public interface IIsPunchItemRelatedQuery : IBaseRequest
+{
+    Guid PunchItemGuid { get; }
+    ProjectDetailsDto ProjectDetailsDto { get; set; }
+}
