89d4f754e665cb4f0ffd93dec29e0bc47b95ddaf
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-08-23 08:23:58 +0200

    MainApiCheckListService.GetManyCheckListsAsync need to call main api endpoint in bulk due to long urls. Made unit tests for this

--- src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/CheckList/MainApiCheckListService.cs
+++ src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/CheckList/MainApiCheckListService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -22,7 +23,8 @@ public class MainApiCheckListService(
     private readonly Uri _baseAddress = new(mainApiOptions.CurrentValue.BaseAddress);
     private readonly bool _recalculateStatusInPcs4 = applicationOptions.CurrentValue.RecalculateStatusInPcs4;
 
-    // Do not pass plant to the GET endpoint for checklist in Main API due to performance. The endpoint has m2m auth, hence it doesn't require plant specific permissions
+    // Do not pass plant to the GET endpoint for checklist in Main API due to performance.
+    // The endpoint has m2m auth, hence it doesn't require plant specific permissions
     public async Task<ProCoSys4CheckList?> GetCheckListAsync(Guid checkListGuid, CancellationToken cancellationToken)
     {
         var url = $"{_baseAddress}CheckList/ForProCoSys5" +
@@ -62,19 +64,46 @@ public class MainApiCheckListService(
         return await mainApiClientForUser.TryQueryAndDeserializeAsync<ChecklistsByPunchGuidInstance>(url, cancellationToken);
     }
 
-    // Do not pass plant to the GET endpoint for many checklists in Main API due to performance. The endpoint has m2m auth, hence it doesn't require plant specific permissions
+    // Do not pass plant to the GET endpoint for many checklists in Main API due to performance.
+    // The endpoint has m2m auth, hence it doesn't require plant specific permissions
     public async Task<List<ProCoSys4CheckList>> GetManyCheckListsAsync(List<Guid> checkListGuids, CancellationToken cancellationToken)
     {
-        var url = $"{_baseAddress}CheckLists/ForProCoSys5" +
+        var baseUrl = $"{_baseAddress}CheckLists/ForProCoSys5" +
                   $"?api-version={_apiVersion}";
 
-        foreach (var checkListGuid in checkListGuids)
-        {
-            url += $"&proCoSysGuids={checkListGuid:N}";
-        }
+        var checkLists = new List<ProCoSys4CheckList>();
 
-        // Execute as application. The get checklists endpoint in Main Api requires
-        // a special role "Checklist.RecalcStatus", which the Azure application registration has
-        return await mainApiClientForApplication.TryQueryAndDeserializeAsync<List<ProCoSys4CheckList>>(url, cancellationToken);
+        var page = 0;
+        // Use relative small page size since checkListGuids are added to querystring of url and maxlength of an url is 2000
+        // length of url with baseAddress + _apiVersion is approx 105 chars.
+        // Each checkListGuid will add "&proCoSysGuids=EB386A05B827044DE0532910000AFBB1" to url. Length of each is 47
+        // Number of checkLists to add is approx ( (2000-105) / 47) = 40
+        var pageSize = 40;
+        IEnumerable<Guid> pageCheckListGuids;
+        do
+        {
+            pageCheckListGuids = checkListGuids.Skip(pageSize * page).Take(pageSize).ToList();
+
+            if (pageCheckListGuids.Any())
+            {
+                var url = baseUrl;
+                foreach (var checkListGuid in pageCheckListGuids)
+                {
+                    url += $"&proCoSysGuids={checkListGuid:N}";
+                }
+
+                // Execute as application. The get checklists endpoint in Main Api requires
+                // a special role "Checklist.RecalcStatus", which the Azure application registration has
+                var checkListsPage
+                    = await mainApiClientForApplication.TryQueryAndDeserializeAsync<List<ProCoSys4CheckList>>(url, cancellationToken);
+                checkLists.AddRange(checkListsPage);
+            }
+
+            page++;
+
+        } while (pageCheckListGuids.Count() == pageSize);
+
+
+        return checkLists;
     }
 }
---
+++ src/tests/Equinor.ProCoSys.Completion.ForeignApiTests/MainApi/CheckList/MainApiCheckListServiceTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Auth.Client;
+using Equinor.ProCoSys.Completion.Domain;
+using Equinor.ProCoSys.Completion.ForeignApi.MainApi.CheckList;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Equinor.ProCoSys.Completion.ForeignApiTests.MainApi.CheckList;
+
+[TestClass]
+public class MainApiCheckListServiceTests
+{
+    private MainApiCheckListService _dut;
+    private IOptionsMonitor<ApplicationOptions> _applicationOptionsMock;
+    private IMainApiClientForApplication _mainApiClientForApplication;
+    private IOptionsMonitor<MainApiOptions> _mainApiOptionsMock;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _applicationOptionsMock = Substitute.For<IOptionsMonitor<ApplicationOptions>>();
+        _applicationOptionsMock.CurrentValue.Returns(new ApplicationOptions { CheckListCacheExpirationMinutes = 1 });
+        _mainApiOptionsMock = Substitute.For<IOptionsMonitor<MainApiOptions>>();
+        _mainApiOptionsMock.CurrentValue.Returns(new MainApiOptions
+        {
+            BaseAddress = "https://backend-xxxxxxxx-yyyyyyyyyy-api-test.radix.zzzzzzz.com/api",
+            ApiVersion = "4.1"
+        });
+
+        _mainApiClientForApplication = Substitute.For<IMainApiClientForApplication>();
+        _mainApiClientForApplication
+            .TryQueryAndDeserializeAsync<List<ProCoSys4CheckList>>(Arg.Any<string>(), Arg.Any<CancellationToken>())
+            .Returns([new ProCoSys4CheckList(Guid.Empty, "R", false, Guid.Empty)]);
+        _dut = new MainApiCheckListService(
+            _mainApiClientForApplication,
+            null!,
+            _mainApiOptionsMock,
+            _applicationOptionsMock);
+    }
+
+    [TestMethod]
+    public async Task GetManyCheckListsAsync_With1CheckListGuid_ShouldCall_MainApiClientOnce()
+    {
+        // Act
+        await _dut.GetManyCheckListsAsync([Guid.NewGuid()], default);
+
+        // Assert
+        await _mainApiClientForApplication.Received(1)
+            .TryQueryAndDeserializeAsync<List<ProCoSys4CheckList>>(Arg.Any<string>(), Arg.Any<CancellationToken>());
+    }
+
+    [TestMethod]
+    public async Task GetManyCheckListsAsync_With42CheckListGuids_ShouldCall_MainApiClientTwice()
+    {
+        // Arrange
+        List<Guid> checkListGuids = [];
+        for (var i = 0; i < 42; i++)
+        {
+            checkListGuids.Add(Guid.NewGuid());
+        }
+
+        // Act
+        await _dut.GetManyCheckListsAsync(checkListGuids, default);
+
+        // Assert
+        await _mainApiClientForApplication.Received(2)
+            .TryQueryAndDeserializeAsync<List<ProCoSys4CheckList>>(Arg.Any<string>(), Arg.Any<CancellationToken>());
+    }
+
+    [TestMethod]
+    public async Task GetManyCheckListsAsync_With42CheckListGuids_ShouldCall_MainApiClientWithCorrectUrls()
+    {
+        // Arrange
+        _mainApiClientForApplication
+            .When(x => x.TryQueryAndDeserializeAsync<List<ProCoSys4CheckList>>(Arg.Any<string>(), Arg.Any<CancellationToken>()))
+            .Do(Callback.Always(callbackInfo =>
+            {
+                var url = callbackInfo.Arg<string>();
+                Assert.IsTrue(url.Length < 2000);
+            }));
+
+        List<Guid> checkListGuids = [];
+        for (var i = 0; i < 42; i++)
+        {
+            checkListGuids.Add(Guid.NewGuid());
+        }
+
+        // Act
+        await _dut.GetManyCheckListsAsync(checkListGuids, default);
+
+        // Assert
+        await _mainApiClientForApplication.Received(2)
+            .TryQueryAndDeserializeAsync<List<ProCoSys4CheckList>>(Arg.Any<string>(), Arg.Any<CancellationToken>());
+    }
+
+    [TestMethod]
+    public async Task GetManyCheckListsAsync_With42CheckListGuids_ShouldReturnResultFromMainApiClientTwice()
+    {
+        // Arrange
+        List<Guid> checkListGuids = [];
+        for (var i = 0; i < 42; i++)
+        {
+            checkListGuids.Add(Guid.NewGuid());
+        }
+
+        // Act
+        var result = await _dut.GetManyCheckListsAsync(checkListGuids, default);
+
+        // Assert
+        Assert.AreEqual(2, result.Count);
+    }
+}
