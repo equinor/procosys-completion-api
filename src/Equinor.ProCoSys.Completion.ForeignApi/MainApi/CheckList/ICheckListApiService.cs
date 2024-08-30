a8261f8da5aaf04c7ef5b60ba2a1342d13747570
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-08-30 08:05:07 +0200

    Extend ICheckListApiService with Task<ProCoSys4CheckListSearchResult> SearchCheckListsAsync

--- src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/CheckList/ICheckListApiService.cs
+++ src/Equinor.ProCoSys.Completion.ForeignApi/MainApi/CheckList/ICheckListApiService.cs
@@ -1,6 +1,6 @@
-﻿using System.Threading.Tasks;
-using System;
+﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Equinor.ProCoSys.Completion.ForeignApi.MainApi.CheckList;
 
@@ -8,5 +8,17 @@ public interface ICheckListApiService
 {
     // Do not pass plant to the GET endpoint for checklist in Main API due to performance. The endpoint has m2m auth, hence it doesn't require plant specific permissions
     Task<ProCoSys4CheckList?> GetCheckListAsync(Guid checkListGuid, CancellationToken cancellationToken);
+    // Do not pass plant to the GET endpoint for checklist in Main API due to performance. The endpoint has m2m auth, hence it doesn't require plant specific permissions
+    Task<ProCoSys4CheckListSearchResult> SearchCheckListsAsync(
+        Guid projectGuid,
+        string? tagNoContains,
+        string? responsibleCode,
+        string? tagRegisterCode,
+        string? tagFunctionCode,
+        string? formularType,
+        int? currentPage,
+        int? itemsPerPage,
+        CancellationToken cancellationToken);
+
     Task RecalculateCheckListStatus(string plant, Guid checkListGuid, CancellationToken cancellationToken);
 }
