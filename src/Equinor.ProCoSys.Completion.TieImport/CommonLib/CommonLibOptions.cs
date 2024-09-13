8b366f9efee1564d12817d80bf085d0e4780d2f6
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-09-13 07:53:26 +0200

    Use AzureAdOptions

--- src/Equinor.ProCoSys.Completion.TieImport/CommonLib/CommonLibOptions.cs
+++ src/Equinor.ProCoSys.Completion.TieImport/CommonLib/CommonLibOptions.cs
@@ -10,10 +10,4 @@ public sealed class CommonLibOptions
     public List<string> SchemaFrom { get; set; } = [];
     [Required]
     public string SchemaTo { get; set; } = string.Empty;
-    [Required]
-    public string ClientId { get; set; } = string.Empty;
-    [Required]
-    public string ClientSecret { get; set; } = string.Empty;
-    [Required]
-    public string TenantId { get; set; } = string.Empty;
 }
