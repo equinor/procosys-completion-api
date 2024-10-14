803e2a15efa511999b9c774715ab2a8b402467ae
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-10-14 13:55:00 +0200

    SendEmailEvent

---
+++ src/Equinor.ProCoSys.Completion.Domain/Events/IntegrationEvents/MailEvents/SendEmailEvent.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections.Generic;
+
+namespace Equinor.ProCoSys.Completion.Domain.Events.IntegrationEvents.MailEvents;
+
+public record SendEmailEvent(List<string> To, string Subject, string Body);
