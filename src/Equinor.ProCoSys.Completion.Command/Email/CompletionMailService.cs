153bf67006d3fc0377e57d8c71ca39c930f9b330
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-10-14 13:54:00 +0200

    Refactor CompletionMailService til create message to sb queue. Not send mail

--- src/Equinor.ProCoSys.Completion.Command/Email/CompletionMailService.cs
+++ src/Equinor.ProCoSys.Completion.Command/Email/CompletionMailService.cs
@@ -2,45 +2,29 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Equinor.ProCoSys.Common.Email;
 using Equinor.ProCoSys.Common.Misc;
 using Equinor.ProCoSys.Common.TemplateTransforming;
+using Equinor.ProCoSys.Completion.Command.MessageProducers;
 using Equinor.ProCoSys.Completion.Domain;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.MailTemplateAggregate;
 using Equinor.ProCoSys.Completion.Domain.AggregateModels.PersonAggregate;
+using Equinor.ProCoSys.Completion.Domain.Events.IntegrationEvents.MailEvents;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Equinor.ProCoSys.Completion.Command.Email;
 
-public class CompletionMailService : ICompletionMailService
+public class CompletionMailService(
+    IPlantProvider plantProvider,
+    IPersonRepository personRepository,
+    IMailTemplateRepository mailTemplateRepository,
+    ITemplateTransformer templateTransformer,
+    IMessageProducer messageProducer,
+    ILogger<CompletionMailService> logger,
+    IOptionsMonitor<ApplicationOptions> options)
+    : ICompletionMailService
 {
-    private readonly IPlantProvider _plantProvider;
-    private readonly IPersonRepository _personRepository;
-    private readonly IMailTemplateRepository _mailTemplateRepository;
-    private readonly ITemplateTransformer _templateTransformer;
-    private readonly IEmailService _emailService;
-    private readonly ILogger<CompletionMailService> _logger;
-    private readonly IOptionsMonitor<ApplicationOptions> _options;
-
-    public CompletionMailService(IPlantProvider plantProvider,
-        IPersonRepository personRepository,
-        IMailTemplateRepository mailTemplateRepository,
-        ITemplateTransformer templateTransformer,
-        IEmailService emailService,
-        ILogger<CompletionMailService> logger,
-        IOptionsMonitor<ApplicationOptions> options)
-    {
-        _plantProvider = plantProvider;
-        _personRepository = personRepository;
-        _mailTemplateRepository = mailTemplateRepository;
-        _templateTransformer = templateTransformer;
-        _emailService = emailService;
-        _logger = logger;
-        _options = options;
-    }
-
-    public async Task SendEmailAsync(
+    public async Task SendEmailEventAsync(
         string templateCode,
         dynamic emailContext,
         List<string> emailAddresses, 
@@ -51,27 +35,30 @@ public class CompletionMailService : ICompletionMailService
             return;
         }
 
-        var mailTemplate = await _mailTemplateRepository.GetNonVoidedByCodeAsync(_plantProvider.Plant, templateCode, cancellationToken);
+        var mailTemplate = await mailTemplateRepository.GetNonVoidedByCodeAsync(plantProvider.Plant, templateCode, cancellationToken);
 
-        var subject = _templateTransformer.Transform(mailTemplate.Subject, emailContext);
-        var body = _templateTransformer.Transform(mailTemplate.Body, emailContext);
+        var subject = templateTransformer.Transform(mailTemplate.Subject, emailContext);
+        var body = templateTransformer.Transform(mailTemplate.Body, emailContext);
 
-        if (_options.CurrentValue.FakeEmail)
+        if (options.CurrentValue.FakeEmail)
         {
             var fakeBody = "This email is sent to you only since fake email is enabled. " +
                            "Without fake email enabled, this mail would have been sent to " +
                            string.Join(",", emailAddresses) + "<br>---<br>" + 
                            body;
             var fakeSubject = $"{subject} (fake email)";
-            var currentUser = await _personRepository.GetCurrentPersonAsync(cancellationToken);
+            var currentUser = await personRepository.GetCurrentPersonAsync(cancellationToken);
             List<string> currentUserEmail = [currentUser.Email];
 
-            await _emailService.SendEmailsAsync(currentUserEmail, fakeSubject, fakeBody, cancellationToken);
-            _logger.LogInformation("Fake email sent. Code='{Code}'. To='{To}'", templateCode, currentUser.Email);
+            await messageProducer.SendEmailEventAsync(new SendEmailEvent(currentUserEmail, fakeSubject, fakeBody), cancellationToken);
+            logger.LogInformation("Event for sending fake email sent. Code='{Code}'. To='{To}'. EMailAddresses='{EMailAddresses}'", 
+                templateCode, 
+                currentUser.Email, 
+                string.Join(",", emailAddresses));
             return;
         }
 
-        await _emailService.SendEmailsAsync(emailAddresses, subject, body, cancellationToken);
-        _logger.LogInformation("Email sent. Code='{Code}'. To='{To}'", templateCode, string.Join(",", emailAddresses));
+        await messageProducer.SendEmailEventAsync(new SendEmailEvent(emailAddresses, subject, body), cancellationToken);
+        logger.LogInformation("Event for sending email sent. Code='{Code}'. To='{To}'", templateCode, string.Join(",", emailAddresses));
     }
 }
