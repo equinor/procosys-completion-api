42a1bc9ff19abc7076bebb4f5c19da6651fc7ce8
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-10-14 13:58:51 +0200

    TestFactory don't have to mock IEmailService since it don't send email now

--- src/tests/Equinor.ProCoSys.Completion.WebApi.IntegrationTests/TestFactory.cs
+++ src/tests/Equinor.ProCoSys.Completion.WebApi.IntegrationTests/TestFactory.cs
@@ -10,7 +10,6 @@ using Equinor.ProCoSys.Auth.Authorization;
 using Equinor.ProCoSys.Auth.Permission;
 using Equinor.ProCoSys.Auth.Person;
 using Equinor.ProCoSys.BlobStorage;
-using Equinor.ProCoSys.Common.Email;
 using Equinor.ProCoSys.Common.Misc;
 using Equinor.ProCoSys.Completion.Domain;
 using Equinor.ProCoSys.Completion.ForeignApi.MainApi.CheckList;
@@ -40,7 +39,6 @@ public class TestFactory : WebApplicationFactory<Program>
     private readonly IPersonApiService _personApiServiceMock = Substitute.For<IPersonApiService>();
     private readonly IPermissionApiService _permissionApiServiceMock = Substitute.For<IPermissionApiService>();
     public readonly ICheckListApiService _checkListApiServiceMock = Substitute.For<ICheckListApiService>();
-    private readonly IEmailService _emailServiceMock = Substitute.For<IEmailService>();
     private readonly TokenCredential _tokenCredentialsMock = Substitute.For<TokenCredential>();
     private readonly IFormularTypeApiService _formularTypeApiService = Substitute.For<IFormularTypeApiService>();
     private readonly IResponsibleApiService _responsibleApiService = Substitute.For<IResponsibleApiService>();
@@ -168,7 +166,6 @@ public Dictionary<string, KnownTestData> SeededData { get; }
             services.AddScoped(_ => _permissionApiServiceMock);
             services.AddScoped(_ => _checkListApiServiceMock);
             services.AddScoped(_ => BlobStorageMock);
-            services.AddScoped(_ => _emailServiceMock);
             services.AddScoped(_ => _formularTypeApiService);
             services.AddScoped(_ => _responsibleApiService);
             services.AddScoped(_ => _tagFunctionApiService);
