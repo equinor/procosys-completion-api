07aae57332810a2a4fe7bbedccede81e737a7fee
Jan Inge Dalsbø <JDALSB@equinor.com>
2024-10-22 09:11:51 +0200

    IPunchItemService.GetPunchItemsByPunchItemGuidsAsync

--- src/Equinor.ProCoSys.Completion.Query/PunchItemServices/IPunchItemService.cs
+++ src/Equinor.ProCoSys.Completion.Query/PunchItemServices/IPunchItemService.cs
@@ -8,6 +8,7 @@ namespace Equinor.ProCoSys.Completion.Query.PunchItemServices;
 public interface IPunchItemService
 {
     Task<PunchItemDetailsDto?> GetPunchItemOrNullByPunchItemGuidAsync(Guid punchItemGuid, CancellationToken cancellationToken);
+    Task<IEnumerable<PunchItemTinyDetailsDto>> GetPunchItemsByPunchItemGuidsAsync(IEnumerable<Guid> punchItemGuids, CancellationToken cancellationToken);
     Task<ProjectDetailsDto?> GetProjectOrNullByPunchItemGuidAsync(Guid punchItemGuid, CancellationToken cancellationToken);
     Task<IEnumerable<PunchItemDetailsDto>> GetByCheckListGuid(Guid checkListGuid, CancellationToken cancellationToken);
 }
