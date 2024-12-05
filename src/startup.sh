cf449d722849e6615de280bebb6a8562167e00e0
Espen Hatleskog <EHA@equinor.com>
2024-12-05 10:12:17 +0100

    rem echo

--- src/startup.sh
+++ src/startup.sh
@@ -2,8 +2,6 @@
 
 # Run database migrations
 echo "Running database migrations..."
-echo "Using AzureId: $AZURE_CLIENT_ID"
-echo "Using DB_CONNECTION_STRING: $DB_CONNECTION_STRING"
 
 /app/migrations/bundle.exe --connection "$DB_CONNECTION_STRING"
 
