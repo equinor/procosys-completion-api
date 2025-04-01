1f3ffe17f218b131f9c5b2a151234812bf736167
Espen Hatleskog <EHA@equinor.com>
2025-04-01 13:46:11 +0200

    Replace ClientSecretCredential with ClientCertificateCredential

--- src/Equinor.ProCoSys.Completion.WebApi/Controllers/MailTemplates/MailTemplatesController.cs
+++ src/Equinor.ProCoSys.Completion.WebApi/Controllers/MailTemplates/MailTemplatesController.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Equinor.ProCoSys.Auth;
+using Equinor.ProCoSys.Common.Email;
 using Equinor.ProCoSys.Completion.Query.MailTemplateQueries.GetAllMailTemplates;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -14,8 +15,13 @@ namespace Equinor.ProCoSys.Completion.WebApi.Controllers.MailTemplates;
 public class MailTemplatesController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly IEmailService _emailService;
 
-    public MailTemplatesController(IMediator mediator) => _mediator = mediator;
+    public MailTemplatesController(IMediator mediator, IEmailService emailService)
+    {
+        _mediator = mediator;
+        _emailService = emailService;
+    }
 
     /// <summary>
     /// Get all mail templates
@@ -27,5 +33,12 @@ public class MailTemplatesController : ControllerBase
     {
         var result = await _mediator.Send(new GetAllMailTemplatesQuery(), cancellationToken);
         return Ok(result);
+    }
+
+    [HttpGet("TestSendMail-Delete-After-Test")]
+    public async Task<ActionResult> SendTestMail(CancellationToken cancellationToken)
+    {
+        await _emailService.SendEmailsAsync(["eha@equinor.com"], "subject", "Body", cancellationToken);
+        return Ok();
     }
 }
